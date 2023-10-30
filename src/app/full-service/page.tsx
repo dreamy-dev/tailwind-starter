771a7938ea1bda274000f135f9450a9a84cda6cf
nataliia-karpenko <nataliia.karpenko@gateb.com>
2023-10-30 13:51:29 +0100

    add full service page

---
+++ src/app/full-service/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Hero from "./Hero";
+import Description from "./Description";
+import Advantages from "./Advantages";
+import References from "./References";
+import Contact from "./Contact";
+import Career from "./Career";
+
+export default function FullService() {
+  return (
+    <>
+      <Hero />
+      <Description />
+      <Advantages />
+      <References />
+      <Contact />
+      <Career />
+    </>
+  );
+}
