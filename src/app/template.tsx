5d16257298ed85ae398940aefe964246a0d595c6
Cécile Hefti <cecile.hefti@outlook.com>
2023-10-31 10:25:26 +0100

    feat: Add Template example file

---
+++ src/app/template.tsx
@@ -0,0 +1,4 @@
+//This is just an example template to remember we can use this functionality
+export default function Template({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
