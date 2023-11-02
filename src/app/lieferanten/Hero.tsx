5b05fe1e3f95c0f1a69cd1d8e6ef292bbd877bfb
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-02 10:16:23 +0100

    feat: Add components to text hero

--- src/app/lieferanten/Hero.tsx
+++ src/app/lieferanten/Hero.tsx
@@ -1,44 +1,24 @@
+import ButtonPrimary from "@/components/elements/ButtonPrimary";
+import H1 from "@/components/typography/H1";
+import Lead from "@/components/typography/Lead";
+
 export default function LieferantenHero() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid grid-cols-12 lg:py-16 lg:px-6">
         <div className="col-span-12 md:col-span-10 font-light text-gray-500 sm:text-lg dark:text-gray-400">
-          <h1 className="mb-8 mt-12 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white tracking-tight">
+          <div className="flex font-normal">
+            <p>Lösungen</p>
+            <span className="px-3">|</span>
+            <p>Schienenfahrzeuge</p>
+          </div>
+          <H1>
             Unsere Lieferanten:
             <br />
             Ein exzellentes Netzwerk
-          </h1>
-          <p className="mb-16 font-light text-gray-500 md:text-lg lg:text-xl dark:text-gray-400">
-            Here at Flowbite we compare a wide range of destinations, flights
-            and hotels to conjure up cheap holidays for you to enjoy, time and
-            time again.
-          </p>
-          <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-start sm:space-y-0 sm:space-x-4">
-            <a
-              href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-            >
-              Sign in Supplier Portal
-            </a>
-            <a
-              href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-              Register
-              <svg
-                className="ml-2 -mr-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </a>
-          </div>
+          </H1>
+          <Lead />
+          <ButtonPrimary />
         </div>
       </div>
     </section>
--- src/components/typography/H1.tsx
+++ src/components/typography/H1.tsx
@@ -1,11 +1,9 @@
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-  PromiseLikeOfReactNode,
-} from "react";
+import { ReactElement } from "react";
 
-export default function H1() {
-  return <h1 className="text-black text-5xl font-bold">Hello there</h1>;
+type Props = {
+  children?: any;
+};
+
+export default function H1({ children }: Props) {
+  return <h1 className="text-black text-5xl font-bold pt-3">{children}</h1>;
 }
