1f3ce02a82f14b55c19b5ba68c1263825a622c7a
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-02 11:57:47 +0100

    feat: Add HeroText section

--- src/app/lieferanten/Hero.tsx
+++ src/app/lieferanten/Hero.tsx
@@ -1,26 +1,5 @@
-import ButtonPrimary from "@/components/elements/ButtonPrimary";
-import H1 from "@/components/typography/H1";
-import Lead from "@/components/typography/Lead";
+import HeroText from "@/components/sections/HeroText";
 
-export default function LieferantenHero() {
-  return (
-    <section className="bg-white dark:bg-gray-900">
-      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid grid-cols-12 lg:py-16 lg:px-6">
-        <div className="col-span-12 md:col-span-10 font-light text-gray-500 sm:text-lg dark:text-gray-400">
-          <div className="flex font-normal">
-            <p>Lösungen</p>
-            <span className="px-3">|</span>
-            <p>Schienenfahrzeuge</p>
-          </div>
-          <H1>
-            Unsere Lieferanten:
-            <br />
-            Ein exzellentes Netzwerk
-          </H1>
-          <Lead />
-          <ButtonPrimary />
-        </div>
-      </div>
-    </section>
-  );
+export default function LieferantenHero(props?: any) {
+  return <HeroText withCTA />;
 }
