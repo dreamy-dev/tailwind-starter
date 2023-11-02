4faa5c4e940cd70a0a4968c03815db6b7b19bc72
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-02 11:42:03 +0100

    refactor: Fix compiler issues

--- src/app/lieferanten/SustainableProcurement.tsx
+++ src/app/lieferanten/SustainableProcurement.tsx
@@ -29,7 +29,7 @@ export default function SustainableProcurement() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
                     clipRule="evenodd"
                   ></path>
@@ -55,7 +55,7 @@ export default function SustainableProcurement() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
                     clipRule="evenodd"
                   ></path>
--- src/components/typography/H1.tsx
+++ src/components/typography/H1.tsx
@@ -1,5 +1,3 @@
-import { ReactElement } from "react";
-
 type Props = {
   children?: any;
 };
