6d3a515fbd5fbb8ae71c30df658b71bb278cec98
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2023-11-03 17:18:25 +0100

    add: section stats which can be reusable in all pages with count up on scroll animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,33 @@ import Stats from "../components/Stats";
 import FactsAndFigures from "@/components/FactsAndFigures";
 
 export default function HomePage() {
+  const statsData = [
+    {
+      text: "Mitarbeiter weltweit",
+      end: 14,
+      suffix: "Tsd",
+    },
+    {
+      text: "Produktionsstandorte",
+      end: 15,
+      suffix: "",
+    },
+    {
+      text: "Arbeitstunden",
+      end: 170,
+      suffix: "Mio",
+    },
+    {
+      text: "Team",
+      end: 1,
+      suffix: "",
+    },
+  ];
   return (
     <>
       <Hero />
       <FactsAndFigures />
-      {/* <Stats /> */}
+      <Stats data={statsData} />
     </>
   );
 }
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,62 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CountUp from "react-countup";
-import ScrollTrigger from "react-scroll-trigger";
+import { motion, useScroll } from "framer-motion";
 
-interface ScrollTriggerProps {
-  onEnter?: () => boolean;
-  onExit?: () => boolean;
+interface StatsProps {
+  data: {
+    text: string;
+    end: number;
+    suffix: string;
+  }[];
 }
 
-const Stats: React.FC<{ props: ScrollTriggerProps }> = () => {
+const Stats: React.FC<StatsProps> = ({ data }) => {
+  const { scrollYProgress } = useScroll();
   const [counterOn, setCounterOn] = useState(false);
-  const handleEnter = () => {
-    setCounterOn(true);
-    return true;
-  };
 
-  const handleExit = () => {
-    setCounterOn(false);
-    return false;
-  };
+  useEffect(() => {
+    const updateCounterOn = (latest: number) => {
+      if (latest > 0.5) {
+        setCounterOn(true);
+      } else {
+        setCounterOn(false);
+      }
+    };
+
+    const unsubscribe = scrollYProgress.onChange(updateCounterOn);
+
+    return () => {
+      unsubscribe();
+    };
+  }, [scrollYProgress]);
+
   return (
-    <ScrollTrigger onEnter={handleEnter} onExit={handleExit}>
+    <>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
       <div className="bg-secondaryBgcLightBlue py-24 sm:py-24">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-4">
-            <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-              <dt className="text-base leading-7 text-gray-600">
-                Mitarbeiter weltweit
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                {counterOn && <CountUp end={14} suffix=" Tsd" />}
-              </dd>
-            </div>
-            <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-              <dt className="text-base leading-7 text-gray-600">
-                Produktionsstandorte
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                {counterOn && <CountUp end={15} />}
-              </dd>
-            </div>
-            <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-              <dt className="text-base leading-7 text-gray-600">
-                Arbeitstunden
-              </dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                {counterOn && <CountUp end={170} suffix=" Mio" />}
-              </dd>
-            </div>
-            <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-              <dt className="text-base leading-7 text-gray-600">Team</dt>
-              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                {counterOn && <CountUp end={1} />}
-              </dd>
-            </div>
+            {data.map((stat, index) => (
+              <div
+                className="mx-auto flex max-w-xs flex-col gap-y-4"
+                key={index}
+              >
+                <dt className="text-base leading-7 text-gray-600">
+                  {stat.text}
+                </dt>
+                <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                  {counterOn && <CountUp end={stat.end} suffix={stat.suffix} />}
+                </dd>
+              </div>
+            ))}
           </dl>
         </div>
       </div>
-    </ScrollTrigger>
+    </>
   );
 };
+
 export default Stats;
