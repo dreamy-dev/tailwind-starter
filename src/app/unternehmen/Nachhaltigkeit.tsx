c9925a0363687f9c36bca8caaafdf1fe6aaa724e
nataliia-karpenko <nataliia.karpenko@gateb.com>
2023-11-06 15:52:14 +0100

    Unternehmen page

--- src/app/unternehmen/Nachhaltigkeit.tsx
+++ src/app/unternehmen/Nachhaltigkeit.tsx
@@ -5,21 +5,17 @@ export default function Nachhaltigkeit() {
         <div className="gap-8 items-center lg:grid lg:grid-cols-2 xl:gap-16">
           <div className="text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              Work with tools you already use
+              Mit Verantwortung in die Zukunft: Engagement in nachhaltigen
+              Mobilitätslösungen
             </h2>
             <p className="mb-8 font-light lg:text-xl">
-              Deliver great service experiences fast - without the complexity of
-              traditional ITSM solutions.Accelerate critical development work,
-              eliminate toil, and deploy changes with ease.
+              Kurzer Beschrieb, Stadler ist Unternehmen, das auf Nachhaltigkeit
+              achtet produziert.
             </p>
-
-            <ul
-              role="list"
-              className="pt-8 my-7 space-y-5 border-t border-gray-200 dark:border-gray-700"
-            >
+            <ul role="list" className="my-7 space-y-5">
               <li className="flex space-x-3">
                 <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
+                  className="flex-shrink-0 w-5 h-5 text-primary dark:text-primary-500"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -27,7 +23,7 @@ export default function Nachhaltigkeit() {
                   <path
                     fill-rule="evenodd"
                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
@@ -36,7 +32,7 @@ export default function Nachhaltigkeit() {
               </li>
               <li className="flex space-x-3">
                 <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
+                  className="flex-shrink-0 w-5 h-5 text-primary dark:text-primary-500"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -44,7 +40,7 @@ export default function Nachhaltigkeit() {
                   <path
                     fill-rule="evenodd"
                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
@@ -53,7 +49,7 @@ export default function Nachhaltigkeit() {
               </li>
               <li className="flex space-x-3">
                 <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
+                  className="flex-shrink-0 w-5 h-5 text-primary dark:text-primary-500"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +57,7 @@ export default function Nachhaltigkeit() {
                   <path
                     fill-rule="evenodd"
                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
@@ -69,128 +65,12 @@ export default function Nachhaltigkeit() {
                 </span>
               </li>
             </ul>
-            <p className="mb-8 font-light lg:text-xl">
-              Deliver great service experiences fast - without the complexity of
-              traditional ITSM solutions.
-            </p>
           </div>
           <img
-            className="hidden mb-4 w-full lg:mb-0 lg:flex rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/features/feature-office-1.png"
+            className="mb-4 w-full lg:mb-0 lg:flex rounded-lg"
+            src="/slider-square.png"
             alt="office feature image"
           />
-        </div>
-
-        <div className="gap-8 items-center lg:grid lg:grid-cols-2 xl:gap-16">
-          <img
-            className="hidden mb-4 w-full lg:mb-0 lg:flex rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/features/feature-office-2.png"
-            alt="office feature image 2"
-          />
-          <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              We invest in the world’s potential
-            </h2>
-            <p className="mb-8 font-light lg:text-xl">
-              Deliver great service experiences fast - without the complexity of
-              traditional ITSM solutions.Accelerate critical development work,
-              eliminate toil, and deploy changes with ease.
-            </p>
-            <ul
-              role="list"
-              className="pt-8 my-7 space-y-5 border-t border-gray-200 dark:border-gray-700"
-            >
-              <li className="flex space-x-3">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
-                  Dynamic reports and dashboards
-                </span>
-              </li>
-              <li className="flex space-x-3">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
-                  Templates for everyone
-                </span>
-              </li>
-              <li className="flex space-x-3">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
-                  Development workflow
-                </span>
-              </li>
-              <li className="flex space-x-3">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
-                  Limitless business automation
-                </span>
-              </li>
-              <li className="flex space-x-3">
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-primary-600 dark:text-primary-500"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">
-                  Knowledge management
-                </span>
-              </li>
-            </ul>
-            <p className="font-light lg:text-xl">
-              Deliver great service experiences fast - without the complexity of
-              traditional ITSM solutions.
-            </p>
-          </div>
         </div>
       </div>
     </section>
--- src/app/unternehmen/WertePrinzipen.tsx
+++ src/app/unternehmen/WertePrinzipen.tsx
@@ -1,45 +1,43 @@
 export default function WertePrinzipen() {
   return (
-    <section className="bg-gray-50">
+    <section className="bg-white">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Designed for business teams like yours
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+            Stadler auf einen Blick
           </h2>
-          <p className="text-gray-500 sm:text-xl dark:text-gray-400">
-            Here at Flowbite we focus on markets where technology, innovation,
-            and capital can unlock long-term value and drive economic growth.
+          <p className="text-gray-500 sm:text-xl">
+            Prinzipien, Werte und Vision von Stadler in Bezug auf das gesamte
+            Unternehmen
           </p>
         </div>
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-gray-50 lg:h-12 lg:w-12">
+            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
               <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                className="mb-2 w-10 h-10 text-primary md:w-12 md:h-12 dark:text-primary-500"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   fill-rule="evenodd"
-                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                  d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
                   clip-rule="evenodd"
                 ></path>
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
-              Marketing
-            </h3>
-            <p className="text-gray-500 dark:text-gray-400">
+            <h3 className="mb-2 text-xl font-bold">Marketing</h3>
+            <p className="text-gray-500">
               Plan it, create it, launch it. Collaborate seamlessly with all the
               organization and hit your marketing goals every month with our
               marketing plan.
             </p>
           </div>
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 dark:bg-primary-900">
               <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                className="mb-2 w-10 h-10 text-primary md:w-12 md:h-12 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -54,9 +52,9 @@ export default function WertePrinzipen() {
             </p>
           </div>
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 dark:bg-primary-900">
               <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                className="mb-2 w-10 h-10 text-primary md:w-12 md:h-12 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -64,7 +62,7 @@ export default function WertePrinzipen() {
                 <path
                   fill-rule="evenodd"
                   d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
                 <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
               </svg>
@@ -78,9 +76,9 @@ export default function WertePrinzipen() {
             </p>
           </div>
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 dark:bg-primary-900">
               <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                className="mb-2 w-10 h-10 text-primary md:w-12 md:h-12 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -89,7 +87,7 @@ export default function WertePrinzipen() {
                 <path
                   fill-rule="evenodd"
                   d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </div>
@@ -100,9 +98,9 @@ export default function WertePrinzipen() {
             </p>
           </div>
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 dark:bg-primary-900">
               <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                className="mb-2 w-10 h-10 text-primary md:w-12 md:h-12 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -119,9 +117,9 @@ export default function WertePrinzipen() {
             </p>
           </div>
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 dark:bg-primary-900">
               <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                className="mb-2 w-10 h-10 text-primary md:w-12 md:h-12 "
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -129,7 +127,7 @@ export default function WertePrinzipen() {
                 <path
                   fill-rule="evenodd"
                   d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </div>
