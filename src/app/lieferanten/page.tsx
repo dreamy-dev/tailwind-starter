4bf5333107a8a661d2e11403c33f18acba1f6fcf
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-06 09:01:00 +0100

    feat: Add quote section

--- src/app/lieferanten/page.tsx
+++ src/app/lieferanten/page.tsx
@@ -1,19 +1,24 @@
 "use client";
 
 import Hero from "./Hero";
-import Quote from "./Quote";
 import Numbers from "./Numbers";
 import ProcurementDescriptions from "./ProcurementDescriptions";
 import CooperationDescription from "./CooperationDescription";
 import SustainableProcurement from "./SustainableProcurement";
 import FAQ from "./FAQ";
 import Contact from "./Contact";
+import FaktenUndZahlen from "../company/FaktenUndZahlen";
+import FaktenZahlen from "../unternehmen/FaktenZahlen";
+import FactsAndFigures from "@/components/FactsAndFigures";
+import Quote from "@/components/sections/Quote";
 
 export default function Lieferanten() {
   return (
     <>
       <Hero />
-      <Numbers />
+      {/* <Numbers /> */}
+      <FactsAndFigures />
+      {/* <FaktenUndZahlen /> */}
       <Quote />
       <ProcurementDescriptions />
       <CooperationDescription />
