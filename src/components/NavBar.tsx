5f8c1ba45a8896cfdaa37d758505dc7fb9ed9dc5
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2023-11-06 14:51:11 +0100

    add: top header and nav sections into header section

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Navbar } from "flowbite-react";
+import ContentWidth from "./layouts/ContentWidth";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -7,54 +7,107 @@ export default function NavBar() {
   const pathname = usePathname();
 
   return (
-    <Navbar
-      className="font-customBlack"
-      style={{
-        marginTop: "30px",
-        marginBottom: "30px",
-        backgroundColor: "white",
-      }}
-    >
-      <Navbar.Brand href="#" as={Link}>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Stadler_Rail_logotype.svg/2560px-Stadler_Rail_logotype.svg.png"
-          className="mr-1 h-4 sm:h-6"
-          alt="Flowbite Logo"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="main-navbar" />
-      <Navbar.Collapse id="main-navbar">
-        <Navbar.Link as={Link} href="/">
-          Home
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/solutions">
-          Solutions
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/career">
-          Career
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/company">
-          Company
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/medien">
-          Media
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/zulieferer">
-          Supplier
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/full-service">
-          Full Service
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/product">
-          Product
-        </Navbar.Link>
-        <Navbar.Link as={Link} href="/lieferanten">
-          Lieferanten
-        </Navbar.Link>
-        {/* <Navbar.Link as={Link} href="/unternehmen">
-          Unternehmen
-        </Navbar.Link> */}
-      </Navbar.Collapse>
-    </Navbar>
+    <nav className="border-gray-200 bg-white dark:bg-gray-800 dark:border-gray-700">
+      <ContentWidth>
+        <div className="col-span-12 max-w-full flex items-center justify-between p-4">
+          <Link href="#" className="flex items-center">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Stadler_Rail_logotype.svg/2560px-Stadler_Rail_logotype.svg.png"
+              className="mr-1 h-4 sm:h-6"
+              alt="Flowbite Logo"
+            />
+          </Link>
+
+          <div
+            className="hidden w-full md:block md:w-auto"
+            id="navbar-solid-bg"
+          >
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+              <li>
+                <Link
+                  href="/"
+                  className="block py-2 pl-3 pr-4 text-gray-900  rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/solutions"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Solutions
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/career"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Career
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/company"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Company
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/medien"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Media
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/zulieferer"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Supplier
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/full-service"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Full Service
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/product"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Product
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/lieferanten"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Lieferanten
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/unternehmen"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Unternehmen
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </ContentWidth>
+    </nav>
   );
 }
