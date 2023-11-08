eacf4277a8aa89dc50124b0d85f55e03265383cb
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-08 08:59:58 +0100

    feat: Add Testimonials section [wip]

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 import ApplicationProcess from "@/components/ApplicationProcess";
-import Benefits from "@/components/Benefits";
 import CTASection from "@/components/CTASection";
 import Figures from "@/components/Figures";
 import Jobs from "@/components/Jobs";
-import Nav from "@/components/Nav";
 import Intro from "@/components/sections/Intro";
-import Testimonial from "@/components/Testimonial";
+import Testimonials from "@/components/sections/Testimonials";
 import CareerHero from "../../components/sections/HeroCareer";
 
 export default function Karriere() {
   return (
     <>
       <CareerHero></CareerHero>
-      {/* <Nav /> */}
       <Intro />
-      <Benefits />
-      <Testimonial />
+      <Testimonials />
       <Figures />
       <Jobs />
       <ApplicationProcess />
