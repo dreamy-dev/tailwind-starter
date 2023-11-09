29a772a840513265d130c21cff81bd9e46cb85c9
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-09 11:37:29 +0100

    feat: Update Company Page Hero

--- src/app/lieferanten/Hero.tsx
+++ src/app/lieferanten/Hero.tsx
@@ -1,5 +1,18 @@
 import HeroText from "@/components/sections/HeroText";
 
 export default function LieferantenHero(props?: any) {
-  return <HeroText withCTA />;
+  return (
+    <HeroText
+      withCTA
+      category="Lieferanten"
+      leadText="Stadler sucht Zulieferer, die mit Innovationskraft, Zuverlässigkeit und hohem Qualitätsanspruch die Schienenmobilität der Zukunft mitgestalten."
+      title={
+        <p>
+          Unsere Lieferanten:
+          <br />
+          Ein exzellentes Netzwerk
+        </p>
+      }
+    />
+  );
 }
