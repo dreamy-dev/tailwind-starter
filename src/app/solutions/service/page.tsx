bc236596158227c35de4d3a14588bd5f96c5783b
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-13 12:02:43 +0100

    fix: Add correct subpages menu

--- src/app/solutions/service/page.tsx
+++ src/app/solutions/service/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import ContentWidth from "@/components/layouts/ContentWidth";
+import Link from "@/components/typography/Link";
+
 export default function Service() {
   return (
-    <>
-      <div>Service Page</div>
-    </>
+    <section>
+      <ContentWidth>
+        <div className="col-span-6" style={{ height: "80px", padding: "20px" }}>
+          <Link href="/solutions/service/full-service">
+            <p>Full service page</p>
+          </Link>
+        </div>
+      </ContentWidth>
+    </section>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,7 +35,10 @@ const navigationMain: NavItems = {
       icon: <IconNav></IconNav>,
       submenu: true,
       submenuItems: [
-        { title: "Schienenfahrt", href: "/solutions/schienenfahrt" },
+        {
+          title: "Schienenfahrzeuge",
+          href: "/solutions/Schienenfahrzeuge",
+        },
         { title: "Signalling", href: "/solutions/signalling" },
         { title: "Service", href: "/solutions/service" },
       ],
@@ -70,7 +73,7 @@ export default function NavBar() {
                   <Link
                     href={item.href}
                     className="flex gap-2 justify-center items-center py-5 pl-3 pr-4 text-primarySolid-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    onClick={() => setIsOpen((isOpen) => !isOpen)}
+                    onMouseEnter={() => setIsOpen((isOpen) => !isOpen)}
                   >
                     {item.title}
                     {item.icon ?? <IconNav />}
