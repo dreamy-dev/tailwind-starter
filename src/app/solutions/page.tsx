d1cbc9fd6187729187e18726ade82a5a1368280d
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-14 11:45:06 +0100

    feat: Add placeholder Referenzen image

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -10,6 +10,11 @@ export default function Solutions() {
       {/* <HeroSolutions></HeroSolutions> */}
       <Hero />
       <TeasersSolution />
+      <img
+        className="mr-4 w-auto h-36 md:w-full md:h-auto rounded-lg"
+        src="/Referenzen.png"
+        alt=""
+      />
       {/* Referenzen */}
     </>
   );
