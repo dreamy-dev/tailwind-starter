cad66e7ea4eae86828fb3605a24c22faae84a868
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2023-11-15 08:28:37 +0100

    add: padding to a slider

--- src/components/ReferenzenCarousel.tsx
+++ src/components/ReferenzenCarousel.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import Button from "./elements/ButtonSecondary";
 import { MotionConfig, motion, MotionProps } from "framer-motion";
 import { useState } from "react";
+import TrainCard from "./TrainCard";
+import ContentWidth from "./layouts/ContentWidth";
 
 interface PaginationProps {
   total: number;
@@ -41,6 +43,20 @@ interface TestimonialMotionProps extends MotionProps {
 }
 
 const TestimonialMotionDiv: React.FC<TestimonialMotionProps> = motion.div;
+const trains = [
+  {
+    name: "Hybrider FLIRT 160 für Flughafen'Transit",
+    image: "/train-c.jpg",
+  },
+  {
+    name: "Ein vollelektrischer FLIRT ywischen DE und NL",
+    image: "/train-c.jpg",
+  },
+  {
+    name: "Zwischen Couda und Alphen: Ein elektrischer Niederflur-FLIRT",
+    image: "/train-c.jpg",
+  },
+];
 
 const images = [
   {
@@ -120,19 +136,25 @@ const TestimonialsCarousel: React.FC = () => {
     }
   };
 
+  const [showTrains, setShowTrains] = useState(false);
+
+  const toggleTrainsVisibility = () => {
+    setShowTrains(!showTrains);
+  };
+
   return (
     <section className=" py-24 bg-primarySolid-50">
       <div className="mb-12 flex justify-center items-center">
         <H2 titleH2="Referenzen"></H2>
       </div>
-      <div className="flex flex-col items-center justify-between ">
+      <div className="flex lg:pl-20 flex-col items-center justify-between ">
         <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
-          <div className="relative ml-20 w-full  max-w-[100%] flex items-center">
+          <div className="relative   w-full  max-w-[100%] flex items-center">
             <motion.div className="flex gap-4 flex-nowrap  overflow-hidden">
               {images.map((image, idx) => (
                 <TestimonialMotionDiv
                   key={image.title}
-                  className="flex flex-col h-auto items-center min-w-[100%]  lg:min-w-[80%] bg-white border border-gray-200  shadow md:flex-row "
+                  className="flex flex-col h-auto items-center min-w-[100%]  lg:min-w-[90%] bg-white border border-gray-200  shadow md:flex-row "
                   animate={{
                     translateX: `calc(-${current * 100}% - ${current}rem)`,
                     opacity: idx === current ? 1 : 0.3,
@@ -184,44 +206,133 @@ const TestimonialsCarousel: React.FC = () => {
           </div>
         </MotionConfig>
       </div>
-      <div className="flex flex-row gap-6 mt-8 justify-end items-center mx-10">
-        <Pagination total={images.length} current={current} />
-        <motion.div
-          className=" flex flex-row gap-4 z-10 "
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+      <div className="relative lg:pl-20  flex flex-row w-full mt-8 justify-beetween items-center ">
+        <button
+          type="button"
+          onClick={toggleTrainsVisibility}
+          className="text-primary items-center text-base flex flex-row gap-2 w-1/2"
         >
-          <button onClick={onPrevClick}>
+          {!showTrains ? (
             <svg
-              width="20"
-              height="20"
+              width="10"
+              height="10"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clipPath="url(#clip0_4995_6652)">
+                <path
+                  d="M20 8.88889H11.1111V0H8.88889V8.88889H0V11.1111H8.88889V20H11.1111V11.1111H20V8.88889Z"
+                  fill="#005893"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_4995_6652">
+                  <rect width="20" height="20" fill="#005893" />
+                </clipPath>
+              </defs>
+            </svg>
+          ) : (
+            <svg
+              width="10"
+              height="10"
               viewBox="0 0 20 20"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M14.8586 20L4 10L14.8586 0L16.4099 1.68288L7.38294 10L16.4099 18.3171L14.8586 20Z"
+                d="M20 8.88892H11.1111H8.88889H0V11.1111H8.88889H11.1111H20V8.88892Z"
                 fill="#005893"
               />
             </svg>
-          </button>
-          <button onClick={onNextClick}>
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.55124 20L4 18.3171L13.0269 10L4 1.68288L5.55124 0L16.4099 10L5.55124 20Z"
-                fill="#005893"
-              />
-            </svg>
-          </button>
-        </motion.div>
+          )}
+          alle einblenden
+        </button>
+
+        <div className=" flex flex-row gap-4 justify-end items-center w-full  p-4">
+          <Pagination total={images.length} current={current} />
+          <motion.div
+            className=" flex flex-row gap-4 z-10 "
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <button onClick={onPrevClick}>
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.8586 20L4 10L14.8586 0L16.4099 1.68288L7.38294 10L16.4099 18.3171L14.8586 20Z"
+                  fill="#005893"
+                />
+              </svg>
+            </button>
+            <button onClick={onNextClick}>
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5.55124 20L4 18.3171L13.0269 10L4 1.68288L5.55124 0L16.4099 10L5.55124 20Z"
+                  fill="#005893"
+                />
+              </svg>
+            </button>
+          </motion.div>
+        </div>
       </div>
+      <ContentWidth>
+        {showTrains && (
+          <div className="col-span-12 max-w-full mx-auto justify-center flex flex-wrap gap-6 mt-2 w-full ">
+            {trains.map((train, idx) => (
+              <div
+                key={idx}
+                className="max-w-sm bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700"
+              >
+                <a href="#">
+                  <img className="" src={train.image} alt="" />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <H3>{train.name}</H3>
+                  </a>
+                  <Link
+                    href="#"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center"
+                  >
+                    <svg
+                      width="10"
+                      height="10"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#clip0_4995_6662)">
+                        <path
+                          d="M7.72573e-07 11.1628L16.338 11.1628L10.9296 18.6047L12.7324 20L20 10L12.7324 -6.35355e-07L10.9296 1.39535L16.338 8.83721L9.75882e-07 8.83721L7.72573e-07 11.1628Z"
+                          fill="#005893"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_4995_6662">
+                          <rect width="20" height="20" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ContentWidth>
     </section>
   );
 };
