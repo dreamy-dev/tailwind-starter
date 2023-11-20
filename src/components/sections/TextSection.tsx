e17518d840f8ca0fe74e95bdd0f00f1f21e69b54
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-20 16:36:52 +0100

    fix: Fix textsection issue

--- src/components/sections/TextSection.tsx
+++ src/components/sections/TextSection.tsx
@@ -5,7 +5,15 @@ export default function TextSection(props?: any, children?: any) {
   return (
     <section className="bg-white dark:bg-gray-900 py-24">
       <SmallWidth>
-        <Text>{children}</Text>
+        <Text>
+          Der einstöckige Intercity- und Regionalzug fährt mit einer maximalen
+          Geschwindigkeit von 160 km/h. Mit durchdachte Ingenieursleistung und
+          Innovation passt sich der FLIRT 160 den spezifischen Anforderungen und
+          Bedingungen von jedem Projekt. Die modulare Bauweise erlaubt
+          individuell konfigurierbare zwei- bis sechsstellige Zugskompositionen
+          in Normal- und Breitspur, und bietet Flexibilität bei der Wahl
+          zwischen elektrischem, Diesel- oder sogar bi-modalem Antrieb.
+        </Text>
       </SmallWidth>
     </section>
   );
