2a4bdc07c9bbb41ebd61bcb0875246d821e783df
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-23 11:49:05 +0100

    fix: Fix carousel import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Video from "@/components/Video";
 import Hero from "../components/ExampleHero";
 import FactsAndFigures from "@/components/sections/FactsAndFigures";
 import HomePageCaroucel from "@/components/HomePageCaroucel";
-import TrainCarousel from "@/components/sections/TrainCarousel";
 import Map from "@/components/sections/Map";
+import TrainCarousel from "@/components/sections/TrainCarousel";
 
 export default function HomePage() {
   return (
