bfe901b769a3b11c27d6359ae1bbf69aee104f36
Cécile Hefti <cecile.hefti@outlook.com>
2023-11-30 09:01:15 +0100

    feat: Add responsiveness for large screens

--- src/components/layouts/ContentWidth.tsx
+++ src/components/layouts/ContentWidth.tsx
@@ -3,5 +3,9 @@ type Props = {
 };
 
 export default function ContentWidth({ children }: Props) {
-  return <div className="mx-20 grid grid-cols-12 gap-6">{children}</div>;
+  return (
+    <div className="mx-20 grid grid-cols-12 gap-6 2xl:max-w-screen-2xl mx-auto">
+      {children}
+    </div>
+  );
 }
--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,40 +1,44 @@
 import H2 from "../typography/H2";
 import Button from "../elements/ButtonSecondary";
 import Text from "../typography/Text";
+import FullWidth from "../layouts/FullWidth";
 
 export default function Video() {
   return (
-    <div className=" bg-white flex flex-col py-24 lg:flex-row justify-between items-center 2xl:max-w-[1760px] 2xl:mx-auto">
-      <div className="max-width: 945px; max-height: 540px; overflow: hidden;">
-        <video
-          className="width: 100%; height: 100%; object-fit: cover; 2xl:max-w-[880px]"
-          controls
-          src={"/MovieStadler.mp4"}
-        />
-      </div>
-      <div className="lg:px-20 ">
-        <div className="">
-          <H2 resetStyles={true} styles="text-4xl font-bold mb-6">
-            Karriere bei Stadler
-          </H2>
+    <div className="bg-white py-24  2xl:max-w-[1760px] 2xl:mx-auto">
+      <FullWidth>
+        <div className="col-span-12 flex flex-col lg:flex-row justify-between items-center">
+          <div className="max-width: 945px; max-height: 540px; overflow: hidden;">
+            <video
+              className="width: 100%; height: 100%; object-fit: cover; 2xl:max-w-[880px]"
+              controls
+              src={"/MovieStadler.mp4"}
+            />
+          </div>
+          <div className="lg:px-20 ">
+            <div className="">
+              <H2 resetStyles={true} styles="text-4xl font-bold mb-6">
+                Karriere bei Stadler
+              </H2>
+              <Text styles="text-xl">
+                Wer alles für die besten Züge der Welt macht, braucht die besten
+                Talente.
+              </Text>
+            </div>
 
-          <Text styles="text-xl">
-            Wer alles für die besten Züge der Welt macht, braucht die besten
-            Talente.
-          </Text>
+            <div className="mt-6">
+              <Button
+                backgroundColorClass="bg-transparent"
+                textColorClass="text-primary"
+                borderColorClass="border-primary"
+                position="left"
+              >
+                Jetzt bewerben
+              </Button>
+            </div>
+          </div>
         </div>
-
-        <div className="mt-6">
-          <Button
-            backgroundColorClass="bg-transparent"
-            textColorClass="text-primary"
-            borderColorClass="border-primary"
-            position="left"
-          >
-            Jetzt bewerben
-          </Button>
-        </div>
-      </div>
+      </FullWidth>
     </div>
   );
 }
