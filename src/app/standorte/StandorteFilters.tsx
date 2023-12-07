368a61e225957a075c80be4a8c48bcc3236433c6
nataliia-karpenko <nataliia.karpenko@gateb.com>
2023-12-07 19:34:40 +0100

    fixing layout for Standorte + adding locations

--- src/app/standorte/StandorteFilters.tsx
+++ src/app/standorte/StandorteFilters.tsx
@@ -3,20 +3,21 @@ export default function StandorteFilters() {
     <div className="flex flex-col items-center justify-center pb-4 space-y-3 md:pb-0 md:mt-4 dark:bg-gray-800 md:flex-row md:space-y-0 md:space-x-4">
       <ul className="flex-wrap hidden text-sm font-medium text-center text-gray-500 md:flex dark:text-gray-400">
         <li className="mb-4 mr-2 lg:mr-4">
-          <a
-            href="#"
-            className="inline-block px-4 py-2 text-base border rounded-full dark:bg-gray-700 dark:border-gray-600 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-          >
-            Produktion
-          </a>
+          <select className="inline-block px-4 py-2 text-base border rounded-full block">
+            <option selected>Land</option>
+            <option value="CH">Schweiz</option>
+            <option value="CA">Canada</option>
+            <option value="FR">Frankreich</option>
+            <option value="DE">Deutschland</option>
+          </select>
         </li>
+
         <li className="mb-4 mr-2 lg:mr-4">
           <a
             href="#"
             className="inline-block px-4 py-2 text-base border rounded-full dark:bg-gray-700 dark:border-gray-600 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-            aria-current="page"
           >
-            Komponenten
+            Produktion / Komponenten
           </a>
         </li>
         <li className="mb-4 mr-2 lg:mr-4">
