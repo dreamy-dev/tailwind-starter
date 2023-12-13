7c7c380614b1b536295e16f73d89ba3affeb98f8
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2023-12-13 16:20:32 +0100

    add: karrere responsive

--- src/components/sections/ReferenzenCarousel.tsx
+++ src/components/sections/ReferenzenCarousel.tsx
@@ -4,7 +4,9 @@ import H2 from "../typography/H2";
 import Link from "next/link";
 import { MotionConfig, motion, MotionProps } from "framer-motion";
 import { useState, useEffect, useRef } from "react";
+import FullWidth from "../layouts/FullWidth";
 import ContentWidth from "../layouts/ContentWidth";
+
 
 interface PaginationProps {
   total: number;
@@ -128,64 +130,64 @@ const TestimonialsCarousel: React.FC = () => {
 
   /*  swipe logic starts here */
 
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  // const containerRef = useRef<HTMLDivElement | null>(null);
 
-  const handleSwipe = (direction: "left" | "right") => {
-    const totalImages = images.length;
-    if (direction === "left" && current > 0) {
-      setCurrent(current - 1);
-    } else if (direction === "right" && current < totalImages - 1) {
-      setCurrent(current + 1);
-    }
-  };
+  // const handleSwipe = (direction: "left" | "right") => {
+  //   const totalImages = images.length;
+  //   if (direction === "left" && current > 0) {
+  //     setCurrent(current - 1);
+  //   } else if (direction === "right" && current < totalImages - 1) {
+  //     setCurrent(current + 1);
+  //   }
+  // };
 
-  useEffect(() => {
-    const container = containerRef.current;
+  // useEffect(() => {
+  //   const container = containerRef.current;
 
-    if (!container) return;
+  //   if (!container) return;
 
-    const handleSwipeStart = (e: TouchEvent | MouseEvent) => {
-      setStartX(e instanceof TouchEvent ? e.touches[0].clientX : e.clientX);
-      setIsDragging(true);
-    };
+  //   const handleSwipeStart = (e: TouchEvent | MouseEvent) => {
+  //     setStartX(e instanceof TouchEvent ? e.touches[0].clientX : e.clientX);
+  //     setIsDragging(true);
+  //   };
 
-    const handleSwipeMove = (e: TouchEvent | MouseEvent) => {
-      if (!isDragging) return;
+  //   const handleSwipeMove = (e: TouchEvent | MouseEvent) => {
+  //     if (!isDragging) return;
 
-      const clientX =
-        e instanceof TouchEvent ? e.touches[0].clientX : e.clientX;
+  //     const clientX =
+  //       e instanceof TouchEvent ? e.touches[0].clientX : e.clientX;
 
-      const distanceX = clientX - startX;
+  //     const distanceX = clientX - startX;
 
-      if (Math.abs(distanceX) > 50) {
-        const direction = distanceX > 0 ? "left" : "right";
-        handleSwipe(direction);
-        setStartX(clientX);
-      }
-    };
+  //     if (Math.abs(distanceX) > 50) {
+  //       const direction = distanceX > 0 ? "left" : "right";
+  //       handleSwipe(direction);
+  //       setStartX(clientX);
+  //     }
+  //   };
 
-    const handleSwipeEnd = () => {
-      setIsDragging(false);
-    };
+  //   const handleSwipeEnd = () => {
+  //     setIsDragging(false);
+  //   };
 
-    container.addEventListener("touchstart", handleSwipeStart);
-    container.addEventListener("touchmove", handleSwipeMove);
-    container.addEventListener("touchend", handleSwipeEnd);
+  //   container.addEventListener("touchstart", handleSwipeStart);
+  //   container.addEventListener("touchmove", handleSwipeMove);
+  //   container.addEventListener("touchend", handleSwipeEnd);
 
-    container.addEventListener("mousedown", handleSwipeStart);
-    container.addEventListener("mousemove", handleSwipeMove);
-    container.addEventListener("mouseup", handleSwipeEnd);
+  //   container.addEventListener("mousedown", handleSwipeStart);
+  //   container.addEventListener("mousemove", handleSwipeMove);
+  //   container.addEventListener("mouseup", handleSwipeEnd);
 
-    return () => {
-      container.removeEventListener("touchstart", handleSwipeStart);
-      container.removeEventListener("touchmove", handleSwipeMove);
-      container.removeEventListener("touchend", handleSwipeEnd);
+  //   return () => {
+  //     container.removeEventListener("touchstart", handleSwipeStart);
+  //     container.removeEventListener("touchmove", handleSwipeMove);
+  //     container.removeEventListener("touchend", handleSwipeEnd);
 
-      container.removeEventListener("mousedown", handleSwipeStart);
-      container.removeEventListener("mousemove", handleSwipeMove);
-      container.removeEventListener("mouseup", handleSwipeEnd);
-    };
-  }, [isDragging, startX]);
+  //     container.removeEventListener("mousedown", handleSwipeStart);
+  //     container.removeEventListener("mousemove", handleSwipeMove);
+  //     container.removeEventListener("mouseup", handleSwipeEnd);
+  //   };
+  // }, [isDragging, startX]);
 
   /*   swipe logic ends here */
 
@@ -206,40 +208,169 @@ const TestimonialsCarousel: React.FC = () => {
   };
 
   return (
-    <section className=" py-24 bg-primarySolid-50">
-      <div className="flex justify-center items-center">
-        <H2>Referenzen</H2>
-      </div>
-      <div className="flex lg:pl-20 flex-col items-center justify-between ">
-        <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
-          <div
-            className="relative w-full max-w-[100%] flex items-center "
-            ref={containerRef}
-          >
-            <motion.div className="flex gap-6 flex-nowrap  overflow-hidden mx-[-10px] px-[10px] my-[-10px] py-[10px]">
-              {images.map((image, idx) => (
-                <TestimonialMotionDiv
-                  key={image.title}
-                  className="flex flex-col items-center min-w-[100%]  shadow-md shadow-greyDarken-200  lg:min-w-[90%] bg-white md:flex-row "
-                  animate={{
-                    translateX: `calc(-${current * 101}% - ${current}rem)`,
-                    opacity: idx === current ? 1 : 0.3,
-                  }}
-                  responsive={responsive}
+    <section className="py-24 bg-primarySolid-50">
+      <FullWidth>
+        <div className="col-span-12 max-w-full lg:pl-20 2xl:pl-0">
+          <div className="flex justify-center items-center">
+            <H2>Referenzen</H2>
+          </div>
+          <div className="flex  flex-col items-center justify-between overflow-hidden">
+            <MotionConfig
+              transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}
+            >
+              <div className="relative w-full max-w-[100%] flex items-center ">
+                <motion.div className="flex gap-6 flex-nowrap mx-[-10px] px-[10px] my-[-10px] py-[10px]">
+                  {images.map((image, idx) => (
+                    <TestimonialMotionDiv
+                      key={image.title}
+                      className="flex flex-col items-stretch min-w-[100%] overflow-hidden shadow-md shadow-greyDarken-200  lg:min-w-[90%] bg-white lg:flex-row "
+                      animate={{
+                        translateX: `calc(-${current * 100}% - ${current}rem)`,
+                        opacity: idx === current ? 1 : 0.3,
+                      }}
+                      responsive={responsive}
+                    >
+                      <img
+                        src={image.img}
+                        alt={image.title}
+                        className="w-full lg:w-1/2 object-fit max-h-[350px] md:max-h-[400px] lg:max-h-[440px]"
+                      />
+
+                      <div className="flex flex-col m-auto p-4 leading-normal max-w-lg ">
+                        <Text styles="mb-6 md:mb-10">{image.name}</Text>
+                        <H3>{image.title}</H3>
+
+                        <Text styles="mb-6 mt-8 md:mb-10 mt-4 md:mt-8">
+                          {image.text}
+                        </Text>
+                        <Link
+                          href="#"
+                          className="inline-flex items-center py-2 text-sm font-medium text-center"
+                        >
+                          <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <g clipPath="url(#clip0_4995_6662)">
+                              <path
+                                d="M7.72573e-07 11.1628L16.338 11.1628L10.9296 18.6047L12.7324 20L20 10L12.7324 -6.35355e-07L10.9296 1.39535L16.338 8.83721L9.75882e-07 8.83721L7.72573e-07 11.1628Z"
+                                fill="#005893"
+                              />
+                            </g>
+                            <defs>
+                              <clipPath id="clip0_4995_6662">
+                                <rect width="20" height="20" fill="white" />
+                              </clipPath>
+                            </defs>
+                          </svg>
+                        </Link>
+                      </div>
+                    </TestimonialMotionDiv>
+                  ))}
+                </motion.div>
+              </div>
+            </MotionConfig>
+          </div>
+          <div className="relative  flex flex-row w-full mt-8 justify-beetween items-center ">
+            <button
+              type="button"
+              onClick={toggleTrainsVisibility}
+              className="text-primary items-center text-base flex flex-row gap-2 w-1/2 whitespace-nowrap"
+            >
+              {!showTrains ? (
+                <svg
+                  width="10"
+                  height="10"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <img
-                    src={image.img}
-                    alt={image.title}
-                    className="w-full md:w-1/2 object-fill max-h-[350px] md:max-h-[400px] lg:max-h-[440px]"
+                  <g clipPath="url(#clip0_4995_6652)">
+                    <path
+                      d="M20 8.88889H11.1111V0H8.88889V8.88889H0V11.1111H8.88889V20H11.1111V11.1111H20V8.88889Z"
+                      fill="#005893"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_4995_6652">
+                      <rect width="20" height="20" fill="#005893" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              ) : (
+                <svg
+                  width="10"
+                  height="10"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M20 8.88892H11.1111H8.88889H0V11.1111H8.88889H11.1111H20V8.88892Z"
+                    fill="#005893"
                   />
+                </svg>
+              )}
+              alle einblenden
+            </button>
 
-                  <div className="flex flex-col m-auto p-4 leading-normal max-w-lg ">
-                    <Text styles="mb-6 md:mb-10">{image.name}</Text>
-                    <H3>{image.title}</H3>
+            <div className=" flex flex-row gap-4 justify-end items-center w-full  p-4">
+              <Pagination total={images.length} current={current} />
+              <motion.div
+                className=" flex flex-row gap-4 z-10 "
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <button onClick={onPrevClick}>
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14.8586 20L4 10L14.8586 0L16.4099 1.68288L7.38294 10L16.4099 18.3171L14.8586 20Z"
+                      fill="#005893"
+                    />
+                  </svg>
+                </button>
+                <button onClick={onNextClick}>
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M5.55124 20L4 18.3171L13.0269 10L4 1.68288L5.55124 0L16.4099 10L5.55124 20Z"
+                      fill="#005893"
+                    />
+                  </svg>
+                </button>
+              </motion.div>
+            </div>
+          </div>
 
-                    <Text styles="mb-6 mt-8 md:mb-10 mt-4 md:mt-8">
-                      {image.text}
-                    </Text>
+          {showTrains && (
+            <div className=" md:grid grid-cols-1 gap-6 md:gap-10 lg:grid-cols-3 xl:gap-28 mt-2 w-full lg:pr-20">
+              {trains.map((train, idx) => (
+                <div
+                  key={idx}
+                  className="max-full justify-center mx-auto md:max-w-md bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <a href="#">
+                    <img className="w-full" src={train.image} alt="" />
+                  </a>
+                  <div className="p-5">
+                    <a href="#">
+                      <H3>{train.name}</H3>
+                    </a>
                     <Link
                       href="#"
                       className="inline-flex items-center py-2 text-sm font-medium text-center"
@@ -265,139 +396,12 @@ const TestimonialsCarousel: React.FC = () => {
                       </svg>
                     </Link>
                   </div>
-                </TestimonialMotionDiv>
-              ))}
-            </motion.div>
-          </div>
-        </MotionConfig>
-      </div>
-      <div className="relative lg:pl-20  flex flex-row w-full mt-8 justify-beetween items-center ">
-        <button
-          type="button"
-          onClick={toggleTrainsVisibility}
-          className="text-primary items-center text-base flex flex-row gap-2 w-1/2 whitespace-nowrap"
-        >
-          {!showTrains ? (
-            <svg
-              width="10"
-              height="10"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clipPath="url(#clip0_4995_6652)">
-                <path
-                  d="M20 8.88889H11.1111V0H8.88889V8.88889H0V11.1111H8.88889V20H11.1111V11.1111H20V8.88889Z"
-                  fill="#005893"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_4995_6652">
-                  <rect width="20" height="20" fill="#005893" />
-                </clipPath>
-              </defs>
-            </svg>
-          ) : (
-            <svg
-              width="10"
-              height="10"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M20 8.88892H11.1111H8.88889H0V11.1111H8.88889H11.1111H20V8.88892Z"
-                fill="#005893"
-              />
-            </svg>
-          )}
-          alle einblenden
-        </button>
-
-        <div className=" flex flex-row gap-4 justify-end items-center w-full  p-4">
-          <Pagination total={images.length} current={current} />
-          <motion.div
-            className=" flex flex-row gap-4 z-10 "
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <button onClick={onPrevClick}>
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M14.8586 20L4 10L14.8586 0L16.4099 1.68288L7.38294 10L16.4099 18.3171L14.8586 20Z"
-                  fill="#005893"
-                />
-              </svg>
-            </button>
-            <button onClick={onNextClick}>
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.55124 20L4 18.3171L13.0269 10L4 1.68288L5.55124 0L16.4099 10L5.55124 20Z"
-                  fill="#005893"
-                />
-              </svg>
-            </button>
-          </motion.div>
-        </div>
-      </div>
-      <ContentWidth>
-        {showTrains && (
-          <div className="col-span-12 max-w-full mx-auto justify-center flex flex-wrap gap-6 mt-2 w-full ">
-            {trains.map((train, idx) => (
-              <div
-                key={idx}
-                className="max-w-sm bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700"
-              >
-                <a href="#">
-                  <img className="" src={train.image} alt="" />
-                </a>
-                <div className="p-5">
-                  <a href="#">
-                    <H3>{train.name}</H3>
-                  </a>
-                  <Link
-                    href="#"
-                    className="inline-flex items-center py-2 text-sm font-medium text-center"
-                  >
-                    <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g clipPath="url(#clip0_4995_6662)">
-                        <path
-                          d="M7.72573e-07 11.1628L16.338 11.1628L10.9296 18.6047L12.7324 20L20 10L12.7324 -6.35355e-07L10.9296 1.39535L16.338 8.83721L9.75882e-07 8.83721L7.72573e-07 11.1628Z"
-                          fill="#005893"
-                        />
-                      </g>
-                      <defs>
-                        <clipPath id="clip0_4995_6662">
-                          <rect width="20" height="20" fill="white" />
-                        </clipPath>
-                      </defs>
-                    </svg>
-                  </Link>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </ContentWidth>
+              ))}
+            </div>
+          )}
+        </div>
+      </FullWidth>
     </section>
   );
 };
