948b52272e4c7f6725d75d8b989e9913df7dc1eb
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2023-12-29 16:32:46 +0100

    add: new sections to schienenfahryeuge page

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -7,32 +7,53 @@ import TeasersSolution from "./TeasersSolution";
 import ReferenzenCarousel from "../../components/sections/ReferenzenCarousel";
 import HeroText from "@/components/sections/HeroText";
 import SubHero from "@/components/sections/SubHero";
+import TeaserImageLeft from "@/components/sections/TeaserImageLeft";
+import TeaserImageRight from "@/components/sections/TeaserImageright";
 
 export default function Solutions() {
   return (
     <>
-      <SubHero imageUrl="/subhero.jpg"></SubHero>
+      {/* <SubHero imageUrl="/subhero.jpg"></SubHero> */}
       <HeroText
         // breadcrumbs={[
         //   { text: "Lösungen", link: "/page" },
         //   { text: "Schienenfahrzeuge", link: "/schienenfahrzeuge" },
         //   { text: "Product", link: "/product" },
         // ]}
-        title="Lorem ipsum dolor sit amet"
-        // leadText="Your lead text here."
+        title="Effiziente und zuverlässige Mobilitätslösungen"
+        leadText="Die Mobilitätslösungen von Stadler sind mehr als die Summe ihrer Einzelteile. Wenn erstklassige Schienenfahrzeuge mit massgeschneiderten Signalling-Lösungen und einem integrierten Service kombiniert werden, dann ist ein effizienter Hochleistungsbahnbetrieb nachhaltig sichergestellt."
         // withCTA={true}
         // buttonText="CTA Button"
       />
-      <SmallWidth>
-        <Lead>
-          Für Stadler endet Kundennähe nicht mit der Auslieferung der Züge.
-          Unsere Mitarbeitenden stehen unseren Kunden als Servicepartner rund um
-          die Uhr mit Rat und Tat zur Seite, um höchste Qualität zu garantieren.
-          Dadurch gewährleisten wir die Zuverlässigkeit und Verfügbarkeit
-          unserer Schienenfahrzeuge über die gesamte Lebensdauer.
-        </Lead>
-      </SmallWidth>
-      <TeasersSolution />
+      <TeaserImageRight
+        imageSrc="Referenzen_TSSSA_Bild-VR.jpg"
+        title="Schienenfahrzeuge"
+        description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
+        buttonText="Mehr dazu"
+        buttonHref="/schienenfahrzeuge"
+        backgroundColor="primaryTrans-50"
+      
+      ></TeaserImageRight>
+      <TeaserImageLeft
+        imageSrc="news-1.jpg"
+        title="Signalling"
+        description="  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor"
+        buttonText="Mehr dazu"
+        buttonHref="/signalling"
+        backgroundColor="primaryTrans-50"
+      
+      ></TeaserImageLeft>
+      <TeaserImageRight
+        imageSrc="Referenzen_TSSSA_Bild-VR.jpg"
+        title="Service"
+        description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
+        buttonText="Mehr dazu"
+        buttonHref="/service"
+        backgroundColor="primaryTrans-50"
+      
+      ></TeaserImageRight>
+
+      {/* <TeasersSolution /> */}
       <ReferenzenCarousel />
     </>
   );
--- src/app/solutions/schienenfahrzeuge/page.tsx
+++ src/app/solutions/schienenfahrzeuge/page.tsx
@@ -2,15 +2,71 @@
 
 import ContentWidth from "@/components/layouts/ContentWidth";
 import Link from "@/components/typography/Link";
+import TeaserImageLeft from "@/components/sections/TeaserImageLeft";
+import TeaserImageRight from "@/components/sections/TeaserImageright";
+import HeroText from "@/components/sections/HeroText";
 
 export default function Schienenfahrzeuge() {
   return (
     <section>
       <ContentWidth>
-        <div className="col-span-6" style={{ height: "80px", padding: "20px" }}>
-          <Link href="/solutions/schienenfahrzeuge/product">
-            <p>Product page</p>
-          </Link>
+        <div className="col-span-12">
+          <HeroText
+            title="Schienenfahrzeuge"
+            leadText="In enger Zusammenarbeit mit Kunden werden innovative, modulare oder massgeschneiderte Schienenfahrzeuge entwickelt."
+          ></HeroText>
+          <TeaserImageRight
+            imageSrc="/image4.png"
+            title="Mainline"
+            description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
+            buttonText="Mehr dazu"
+            buttonHref="/mainline"
+            backgroundColor="blueDark"
+            titleColor="text-white"
+            textColor="text-white"
+            leadTextColor="text-white"
+          ></TeaserImageRight>
+          <TeaserImageLeft
+            imageSrc="/ImageUrban.png"
+            title="Urban"
+            description="  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor"
+            buttonText="Mehr dazu"
+            buttonHref="/urban"
+            backgroundColor="blueDark"
+            titleColor="text-white"
+            textColor="text-white"
+            leadTextColor="text-white"
+          ></TeaserImageLeft>
+          <TeaserImageRight
+            imageSrc="/Image123.png"
+            title="Locomotive"
+            description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
+            buttonText="Mehr dazu"
+            buttonHref="/locomotive"
+            backgroundColor="blueDark"
+            titleColor="text-white"
+            textColor="text-white"
+            leadTextColor="text-white"
+          ></TeaserImageRight>
+          <TeaserImageLeft
+            imageSrc="/ImageTailor-cut.png"
+            title="Tailor Made"
+            description="  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor"
+            buttonText="Mehr dazu"
+            buttonHref="/tailor-made"
+            backgroundColor="blueDark"
+            titleColor="text-white"
+            textColor="text-white"
+            leadTextColor="text-white"
+          ></TeaserImageLeft>
+           <div
+            className="col-span-6"
+            style={{ height: "80px", padding: "20px" }}
+          >
+            <Link href="/solutions/schienenfahrzeuge/product">
+              <p>Product page</p>
+            </Link>
+          </div> 
         </div>
       </ContentWidth>
     </section>
