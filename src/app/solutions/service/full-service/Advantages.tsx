39162751c2b8f20adbe43b0e6d1e8cdef881e8d1
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-01-09 09:20:01 +0100

    feat: Adjust title and spacing

--- src/app/solutions/service/full-service/Advantages.tsx
+++ src/app/solutions/service/full-service/Advantages.tsx
@@ -1,11 +1,13 @@
+import H2 from "@/components/typography/H2";
+import H3 from "@/components/typography/H3";
+import H4 from "@/components/typography/H4";
+
 export default function Advantages() {
   return (
     <section className="bg-gray-50">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Die Vorteile auf einen Blick
-          </h2>
+          <H2>Die Vorteile auf einen Blick</H2>
         </div>
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div>
@@ -23,11 +25,9 @@ export default function Advantages() {
                 ></path>
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
-              Weltweit unterwegs, lokal verankert
-            </h3>
+            <H4>Weltweit unterwegs, lokal verankert</H4>
             <p className="text-gray-500 dark:text-gray-400">
-              Die 80 Service Standorte weltweit stellen für eine lokale
+            Die 80 Service Standorte weltweit stellen für eine lokale
               Betreuung vor Ort sicher
             </p>
           </div>
@@ -42,9 +42,9 @@ export default function Advantages() {
                 <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
+            <H4 >
               Aus einer Hand
-            </h3>
+            </H4>
             <p className="text-gray-500 dark:text-gray-400">
               Individuelles Komplett-Angebot mit zentralem Ansprechpartner für
               alle Anliegen betreffend Instandhaltung
@@ -66,9 +66,9 @@ export default function Advantages() {
                 <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
+            <H4>
               Kosten im Griff
-            </h3>
+            </H4>
             <p className="text-gray-500 dark:text-gray-400">
               Mit der regelmässigen Wartung wird die Flotte zuverlässiger,
               verfügbarer und sicherer. Fixe Kilometerpauschale und definierte
@@ -91,9 +91,9 @@ export default function Advantages() {
                 ></path>
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
+            <H4>
               Partner auf Augenhöhe
-            </h3>
+            </H4>
             <p className="text-gray-500 dark:text-gray-400">
               Wir beteiligen uns bei der Risikoteilung und sorgen für Garantie:
               Verfügbarkeits- und Zuverlässigkeitsgarantie sowie
@@ -111,9 +111,9 @@ export default function Advantages() {
                 <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
+            <H4>
               Aus einer Hand
-            </h3>
+            </H4>
             <p className="text-gray-500 dark:text-gray-400">
               Individuelles Komplett-Angebot mit zentralem Ansprechpartner für
               alle Anliegen betreffend Instandhaltung
@@ -134,9 +134,9 @@ export default function Advantages() {
                 ></path>
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
+            <H4>
               Kosten im Griff
-            </h3>
+            </H4>
             <p className="text-gray-500 dark:text-gray-400">
               Mit der regelmässigen Wartung wird die Flotte zuverlässiger,
               verfügbarer und sicherer. Fixe Kilometerpauschale und definierte
