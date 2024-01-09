83ce55202f88e6d3533e476cdbfe507bbb93bc77
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-01-09 17:10:59 +0100

    hover cards component and publications block on investor relations

--- src/app/investors/page.tsx
+++ src/app/investors/page.tsx
@@ -7,9 +7,95 @@ import Subscribe from "@/components/sections/Subscribe";
 import Publikationen from "@/components/sections/Publikationen";
 import CorporateGorvernance from "../unternehmen/CorporateGovernance";
 import InvestorHero from "./Hero";
-import Publications from "./Publications";
 import TextBlock from "./TextBlock";
 import SubHero from "@/components/sections/SubHero";
+
+const publications = [
+  {
+    img: "/publications/publ-card-1.png",
+    title: "Halbjahresbericht 2023",
+    text: "Unser Erfolgsmodell FLIRT bewegt täglich Menschen und Länder. ",
+    href:
+      "https://www.stadlerrail.com/media/pdf/stadler_halbjahresbericht%202023_de_final.pdf",
+  },
+  {
+    img: "/publications/publ-card-2.png",
+    title: "Geschäftsbericht 2022",
+    text: "Unser Erfolgsmodell FLIRT bewegt täglich Menschen und Länder. ",
+    href:
+      "https://www.stadlerrail.com/media/pdf/stadler_halbjahresbericht%202023_de_final.pdf",
+  },
+  {
+    img: "/publications/publ-card-4.png",
+    title: "Nachhaltigkeitsbericht 2021",
+    text: "Unser Erfolgsmodell FLIRT bewegt täglich.",
+    href:
+      "https://www.stadlerrail.com/media/pdf/stadler_nachhaltigkeitsbericht_2022_de_web_v5.pdf",
+  },
+];
+
+const tableData = [
+  {
+    publication: "Geschäftsbericht 2021",
+    date: "13. März 2021",
+    link: "https://www.stadlerrail.com/media/pdf/web_stadler_gb_2021_de.pdf",
+  },
+  {
+    publication: " Halbjahresbericht 2021",
+    date: "13. May 2021",
+    link:
+      "https://www.stadlerrail.com/media/pdf/stadler_halbjahresbericht%202021_de_final.pdf",
+  },
+  {
+    publication: "Geschäftsbericht 2020",
+    date: "22. May 2020",
+    link:
+      "https://www.stadlerrail.com/media/pdf/halbjahresbericht%202020_de.pdf",
+  },
+  {
+    publication: "Halbjahresbericht 2020",
+    date: "24. December 2020",
+    link:
+      "https://www.stadlerrail.com/media/pdf/halbjahresbericht%202020_de.pdf",
+  },
+  {
+    publication: " Geschäftsbericht 2019",
+    date: "15. August 2019",
+    link: "https://www.stadlerrail.com/media/pdf/web_stadler_rail_gb19_de.pdf",
+  },
+  {
+    publication: "Halbjahresbericht 2019",
+    date: "1. January 2019",
+    link:
+      "https://www.stadlerrail.com/media/pdf/2019_0903_halbjahresbericht%202019_de.pdf",
+  },
+  {
+    publication: "Finanzbericht 2018",
+    date: "10. Juli 2018",
+    link: "https://www.stadlerrail.com/media/pdf/finanzbericht_2018_de.pdf",
+  },
+];
+
+const presentations = [
+  {
+    img: "/presentation.png",
+    title: "Präsentation Halbjahresergebnisse 2023",
+    href:
+      "https://www.stadlerrail.com/media/pdf/stadler%20rail%20-%20half-year%202023%20presentation_f_de.pdf",
+  },
+  {
+    img: "/presentation.png",
+    title: "Presentation Full Year Results 2022",
+    href:
+      "https://www.stadlerrail.com/media/pdf/stadler%20rail%20-%20half-year%202023%20presentation_f_de.pdf",
+  },
+  {
+    img: "/presentation.png",
+    title: "Präsentation Capital Markets Day 2022",
+    href:
+      "https://www.stadlerrail.com/media/pdf/stadler%20rail%20-%20half-year%202023%20presentation_f_de.pdf",
+  },
+];
 
 const facts = [
   {
@@ -69,6 +155,39 @@ const figures = [
     duration: 3,
   },
 ];
+
+// const generalversammlungList = [
+//   {
+//     title: "Halbjahresbericht 2023",
+//     image: "/cover_thumbnail_web.jpg",
+//     download: "Download",
+//     href: "/",
+//   },
+//   {
+//     title: "Halbjahresbericht 2023",
+//     image: "/cover_thumbnail_web.jpg",
+//     download: "Download",
+//     href: "/",
+//   },
+//   {
+//     title: "Halbjahresbericht 2023",
+//     image: "/cover_thumbnail_web.jpg",
+//     download: "Download",
+//     href: "/",
+//   },
+//   {
+//     title: "Halbjahresbericht 2023",
+//     image: "/cover_thumbnail_web.jpg",
+//     download: "Download",
+//     href: "/",
+//   },
+//   {
+//     title: "Halbjahresbericht 2023",
+//     image: "/cover_thumbnail_web.jpg",
+//     download: "Download",
+//     href: "/",
+//   },
+// ];
 export default function Investors() {
   return (
     <section>
@@ -76,11 +195,26 @@ export default function Investors() {
       {/* Header kann reiner TextHero sein mit Button der auf die letzte Publikation verweist, alle Publikationen zusammen, vlt ein Bild von publikation? */}
       <InvestorHero />
       {/* News & Events -> links Teaser von letzten News, rechts Teaser zu Events, beide verweisen auf Unterseiten */}
-      <Publikationen></Publikationen>
+      <Publikationen
+        tableData={tableData}
+        publications={publications}
+        title="Publikationen"
+      ></Publikationen>
+      <Publikationen
+        tableData={tableData}
+        publications={presentations}
+        title="Präsentation"
+        showTable={false}
+      ></Publikationen>
       {/* Letzte Publikationen -> 4 letzte Publikationen als Cards anzeigen
       https://flowbite.com/docs/components/card/#horizontal-card
       und der Rest als Collapsible anzeigen mit den restlichen Publikationen
        */}
+      {/* <Generalversammlung
+        title="Finanzberichte"
+        lead="Bitte beachten Sie die neuesten Finanzberichte und erfahren Sie mehr über kommende Anlässe."
+        list={generalversammlungList}
+      /> */}
       <FactsAndFigures data1={facts} data2={figures} title="Daten & Fakten" />
       {/* Warum investieren -> Text */}
       {/* Geschätsmodell -> Text */}
--- src/components/sections/Publikationen.tsx
+++ src/components/sections/Publikationen.tsx
@@ -70,8 +70,27 @@ const tableData = [
     link: "https://www.stadlerrail.com/media/pdf/finanzbericht_2018_de.pdf",
   },
 ];
-
-export default function Publicationen() {
+interface StatsProps {
+  publications: {
+    img?: string;
+    title?: string;
+    text?: string;
+    href?: string;
+  }[];
+  tableData: {
+    publication?: string;
+    date?: string;
+    link?: string;
+  }[];
+  title: string;
+  showTable?: boolean;
+}
+const Publicationen: React.FC<StatsProps> = ({
+  publications,
+  tableData,
+  title,
+  showTable = true,
+}) => {
   const [showPublications, setShowPublications] = useState(false);
 
   const toggleTrainsVisibility = () => {
@@ -83,7 +102,7 @@ export default function Publicationen() {
       <ContentWidth>
         <div className="col-span-12 w-full">
           <div className="text-start">
-            <H2>Publikationen</H2>
+            <H2>{title}</H2>
           </div>
           <div className="w-full mx-auto justify-center items-center grid grid-cols-1 gap-x-10 gap-y-10 lg:max-w-none lg:grid-cols-3">
             {publications.map((publication) => (
@@ -102,7 +121,7 @@ export default function Publicationen() {
                   <a href={publication.href}>
                     <H3>{publication.title}</H3>
                   </a>
-                  <Link
+                  <a
                     href={publication.href}
                     target="_blank"
                     className="inline-flex items-center pt-2 text-sm font-medium text-center"
@@ -126,55 +145,57 @@ export default function Publicationen() {
                         </clipPath>
                       </defs>
                     </svg>
-                  </Link>
+                  </a>
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="relative flex flex-row w-full mt-8 justify-beetween items-center ">
-          <button
-            type="button"
-            onClick={toggleTrainsVisibility}
-            className="text-primary items-center text-base flex flex-row gap-2 whitespace-nowrap"
-          >
-            {!showPublications ? (
-              <svg
-                width="10"
-                height="10"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clipPath="url(#clip0_4995_6652)">
+        {showTable && (
+          <div className="relative flex flex-row w-full mt-8 justify-beetween items-center ">
+            <button
+              type="button"
+              onClick={toggleTrainsVisibility}
+              className="text-primary items-center text-base flex flex-row gap-2 whitespace-nowrap"
+            >
+              {!showPublications ? (
+                <svg
+                  width="10"
+                  height="10"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clipPath="url(#clip0_4995_6652)">
+                    <path
+                      d="M20 8.88889H11.1111V0H8.88889V8.88889H0V11.1111H8.88889V20H11.1111V11.1111H20V8.88889Z"
+                      fill="#005893"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_4995_6652">
+                      <rect width="20" height="20" fill="#005893" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              ) : (
+                <svg
+                  width="10"
+                  height="10"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
-                    d="M20 8.88889H11.1111V0H8.88889V8.88889H0V11.1111H8.88889V20H11.1111V11.1111H20V8.88889Z"
+                    d="M20 8.88892H11.1111H8.88889H0V11.1111H8.88889H11.1111H20V8.88892Z"
                     fill="#005893"
                   />
-                </g>
-                <defs>
-                  <clipPath id="clip0_4995_6652">
-                    <rect width="20" height="20" fill="#005893" />
-                  </clipPath>
-                </defs>
-              </svg>
-            ) : (
-              <svg
-                width="10"
-                height="10"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M20 8.88892H11.1111H8.88889H0V11.1111H8.88889H11.1111H20V8.88892Z"
-                  fill="#005893"
-                />
-              </svg>
-            )}
-            alle einblenden
-          </button>
-        </div>
+                </svg>
+              )}
+              alle einblenden
+            </button>
+          </div>
+        )}
         {showPublications && (
           <div className="col-span-12">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -218,4 +239,6 @@ export default function Publicationen() {
       </ContentWidth>
     </section>
   );
-}
+};
+
+export default Publicationen;
