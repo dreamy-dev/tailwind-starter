8176a73300a9610433034f7e781c70f973d686a1
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-01-10 10:05:16 +0100

    fix: image left right sections on all pages

--- src/app/solutions/schienenfahrzeuge/page.tsx
+++ src/app/solutions/schienenfahrzeuge/page.tsx
@@ -5,6 +5,9 @@ import Link from "@/components/typography/Link";
 import TeaserImageLeft from "@/components/sections/TeaserImageLeft";
 import TeaserImageRight from "@/components/sections/TeaserImageright";
 import HeroText from "@/components/sections/HeroText";
+import Text from "@/components/typography/Text";
+import Button from "@/components/elements/ButtonSecondary";
+import H2 from "@/components/typography/H2";
 
 export default function Schienenfahrzeuge() {
   return (
@@ -15,50 +18,76 @@ export default function Schienenfahrzeuge() {
           title="Schienenfahrzeuge"
           leadText="In enger Zusammenarbeit mit Kunden werden innovative, modulare oder massgeschneiderte Schienenfahrzeuge entwickelt."
         ></HeroText>
-        <TeaserImageRight
-          imageSrc="/image4.png"
-          title="Mainline"
-          description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
-          buttonText="Mehr dazu"
-          buttonHref="/solutions/schienenfahrzeuge/product"
-          backgroundColor="blueDark"
-          titleColor="text-white"
-          textColor="text-white"
-          leadTextColor="text-white"
-        ></TeaserImageRight>
-        <TeaserImageLeft
-          imageSrc="/ImageUrban.png"
-          title="Urban"
-          description="  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor"
-          buttonText="Mehr dazu"
-          buttonHref="/solutions/schienenfahrzeuge/product"
-          backgroundColor="blueDark"
-          titleColor="text-white"
-          textColor="text-white"
-          leadTextColor="text-white"
-        ></TeaserImageLeft>
-        <TeaserImageRight
-          imageSrc="/Image123.png"
-          title="Locomotive"
-          description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
-          buttonText="Mehr dazu"
-          buttonHref="/solutions/schienenfahrzeuge/product"
-          backgroundColor="blueDark"
-          titleColor="text-white"
-          textColor="text-white"
-          leadTextColor="text-white"
-        ></TeaserImageRight>
-        <TeaserImageLeft
-          imageSrc="/ImageTailor-cut.png"
-          title="Tailor Made"
-          description="  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor"
-          buttonText="Mehr dazu"
-          buttonHref="/solutions/schienenfahrzeuge/product"
-          backgroundColor="blueDark"
-          titleColor="text-white"
-          textColor="text-white"
-          leadTextColor="text-white"
-        ></TeaserImageLeft>
+        <div className="py-12 bg-stadlergradient">
+          <TeaserImageRight imageSrc="/test-img-1.png">
+            <div>
+              <H2 styles="text-white">Mainline</H2>
+              <Text styles="mt-4 text-xl font-normal text-gray-800 text-white">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum.
+              </Text>
+            </div>
+            <Button
+              styles="text-white"
+              position="left"
+              href="/solutions/schienenfahrzeuge/product"
+            >
+              Mehr dazu
+            </Button>
+          </TeaserImageRight>
+          <TeaserImageLeft imageSrc="/test-img-1.png">
+            <div>
+              <H2 styles="text-white">Urban</H2>
+              <Text styles="mt-4 text-xl font-normal text-white">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor
+              </Text>
+            </div>
+            <Button
+              styles="text-white"
+              position="left"
+              href="/solutions/schienenfahrzeuge/product"
+            >
+              Mehr dazu
+            </Button>
+          </TeaserImageLeft>
+          <TeaserImageRight imageSrc="/test-img-1.png">
+            <div>
+              <H2 styles="text-white">Locomotive</H2>
+              <Text styles="mt-4 text-xl font-normal text-gray-800 text-white">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum.
+              </Text>
+            </div>
+            <Button
+              styles="text-white"
+              position="left"
+              href="/solutions/schienenfahrzeuge/product"
+            >
+              Mehr dazu
+            </Button>
+          </TeaserImageRight>
+          <TeaserImageLeft imageSrc="/test-img-1.png">
+            <div>
+              <H2 styles="text-white">Tailor Made</H2>
+              <Text styles="mt-4 text-xl font-normal text-white">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor
+              </Text>
+            </div>
+            <Button
+              styles="text-white"
+              position="left"
+              href="/solutions/schienenfahrzeuge/product"
+            >
+              Mehr dazu
+            </Button>
+          </TeaserImageLeft>
+        </div>
         <div className="col-span-6" style={{ height: "80px", padding: "20px" }}>
           <Link href="/solutions/schienenfahrzeuge/product">
             <p>Product page</p>
--- src/components/sections/TeaserImageright.tsx
+++ src/components/sections/TeaserImageright.tsx
@@ -5,103 +5,36 @@ import SmallWidth from "../layouts/SmallWidth";
 
 interface TeaserImageLeftProps {
   imageSrc?: string;
-  title?: string;
-  description?: string;
-  listItems?: string[];
-  text?: string;
-  buttonText?: string;
-  buttonHref?: string;
-  backgroundColor?: string;
-  titleColor?: string;
-  textColor?: string;
-  leadTextColor?: string;
-  listColor?: string;
+  children?: React.ReactNode;
+ 
 }
 
 const TeaserImageRight: React.FC<TeaserImageLeftProps> = ({
   imageSrc,
-  title,
-  description,
-  listItems,
-  text,
-  buttonText,
-  buttonHref,
-  backgroundColor,
-  titleColor,
-  textColor,
-  listColor,
-  leadTextColor,
+  children,
+
+ 
 }) => {
   return (
-    <section className={`bg-${backgroundColor} antialiased py-20`}>
+    <section className="antialiased py-12">
       <SmallWidth>
-        <div className="grid grid-cols-1 gap-8 lg:gap-16 lg:grid-cols-2">
+        <div className="grid grid-cols-1 gap-8 lg:gap-16 lg:grid-cols-2 items-center">
           <div>
             <div id="deviceTabContent" className="mt-8">
               <div className="">
                 <div className="space-y-4 sm:space-y-6 lg:space-y-8">
-                  <div>
-                    <H2 styles={`${titleColor}`}>{title}</H2>
-                    <Text
-                      styles={`mt-4 text-xl font-normal text-gray-800 ${leadTextColor}`}
-                    >
-                      {description}
-                    </Text>
-                  </div>
-
-                  <div className="pt-4 sm:pt-6 lg:pt-8">
-                    <ul className="space-y-4">
-                      {listItems &&
-                        listItems.map((item, index) => (
-                          <li key={index} className="flex items-center gap-2.5">
-                            <div
-                              className={`inline-flex items-center justify-center w-5 h-5 rounded-full bg-${backgroundColor}-100 text-${backgroundColor}-600 shrink-0 dark:bg-${backgroundColor}-900 dark:text-${backgroundColor}-500`}
-                            >
-                              <svg
-                                aria-hidden="true"
-                                className="w-3.5 h-3.5"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                              >
-                                <path
-                                  fillRule="evenodd"
-                                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                  clipRule="evenodd"
-                                />
-                              </svg>
-                            </div>
-                            <span className="text-base font-medium text-gray-900 dark:text-white">
-                              {item}
-                            </span>
-                          </li>
-                        ))}
-                    </ul>
-
-                    <Text
-                      styles={`mt-6 text-xl font-normal text-gray-800 ${leadTextColor}`}
-                    >
-                      {text}
-                    </Text>
-                  </div>
-
-                  <ButtonPrimary
-                    position="left"
-                    buttonText={buttonText}
-                    href={buttonHref}
-                  />
+                  {children}
+                
                 </div>
               </div>
             </div>
           </div>
 
-       
-            <img
-              src={imageSrc}
-              className="object-cover object-left h-[572px]"
-              alt=""
-            />
-        
+          <img
+            src={imageSrc}
+            className="object-cover object-center w-full"
+            alt=""
+          />
         </div>
       </SmallWidth>
     </section>
