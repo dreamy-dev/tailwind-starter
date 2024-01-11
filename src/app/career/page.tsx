086e06de064d0b94e6cffd3c5cb19d03bf5d3b14
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-01-11 15:29:52 +0100

    fixes for Karriere and Lieferanten pages

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -8,12 +8,76 @@ import Testimonials from "@/components/sections/Testimonials";
 import CareerHero from "./HeroCareer";
 import TeaserSuccessStories from "@/components/sections/TeaserSuccessStories";
 import ContentWidth from "@/components/layouts/ContentWidth";
+import HeroText from "@/components/sections/HeroText";
+
+const facts = [
+  {
+    text: "Nettoerlös in CHF",
+    start: 0,
+    end: 3.8,
+    suffix: " Mrd.",
+    duration: 3,
+    separator: " ",
+    decimals: 1,
+    decimal: ".",
+  },
+  {
+    text: "Auftragsbestand in CHF",
+    start: 0,
+    end: 22,
+    suffix: " Mrd.",
+    duration: 3,
+  },
+  {
+    text: "Auftragseingang in CHF",
+    start: 0,
+    end: 8.6,
+    suffix: " Mrd.",
+    separator: " ",
+    decimals: 1,
+    decimal: ".",
+    duration: 3,
+  },
+];
+
+const figures = [
+  {
+    text: "Anzahl Mitarbeitende",
+    start: 0,
+    end: 13.743,
+    separator: " ",
+    decimals: 3,
+    decimal: "'",
+    duration: 3,
+  },
+  {
+    text: "Durchschnittliche Verfügbarkeit von Schienenfahrzeugen",
+    start: 0,
+    end: 98,
+    suffix: "%",
+    prefix: "&gt;",
+    duration: 3,
+  },
+  {
+    text: "Anzahl ausgelieferte Schienenfahrzeuge",
+    start: 0,
+    end: 11.749,
+    separator: " ",
+    decimals: 3,
+    decimal: "'",
+    duration: 3,
+  },
+];
 
 export default function Karriere() {
   return (
     <>
       <CareerHero></CareerHero>
       <Intro />
+      <HeroText
+        title="Karriere bei Uns"
+        leadText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
       <ContentWidth>
         {" "}
         <div className="mt-20 col-span-12 max-w-full">
@@ -26,7 +90,8 @@ export default function Karriere() {
       </ContentWidth>
 
       <Testimonials />
-      {/* <FactsAndFigures /> */}
+
+      <FactsAndFigures data1={facts} data2={figures} title="Daten & Fakten" />
       <Jobs />
       {/* <ApplicationProcess /> */}
       <TeaserSuccessStories />
--- src/components/sections/TeaserDownload.tsx
+++ src/components/sections/TeaserDownload.tsx
@@ -47,19 +47,33 @@ const TeaserDownload: React.FC<TeaserDownloadProps> = ({
                     key={index}
                   >
                     <p className="mr-10">{item.text}</p>
-                    <Button
-                      textColorClass="text-primary"
-                      borderColorClass="border-primary"
-                      styles="flex gap-2"
-                      iconAfter={
-                        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M14.2257 12.6027V14.8206H1.77433V12.6027H0V16.5949H16V12.6027H14.2257Z" fill="#005893"/>
-                          <path d="M14.2878 7.54142L13.2099 6.13083L8.87164 9.44882V0.625977H7.09731V9.44882L2.75465 6.13083L1.67675 7.54142L7.98447 12.3632L14.2878 7.54142Z" fill="#005893"/>
+                    <div className="min-w-[14rem]">
+                      <Button
+                        textColorClass="text-primary"
+                        borderColorClass="border-primary"
+                        styles="flex gap-2"
+                        iconAfter={
+                          <svg
+                            width="16"
+                            height="17"
+                            viewBox="0 0 16 17"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M14.2257 12.6027V14.8206H1.77433V12.6027H0V16.5949H16V12.6027H14.2257Z"
+                              fill="#005893"
+                            />
+                            <path
+                              d="M14.2878 7.54142L13.2099 6.13083L8.87164 9.44882V0.625977H7.09731V9.44882L2.75465 6.13083L1.67675 7.54142L7.98447 12.3632L14.2878 7.54142Z"
+                              fill="#005893"
+                            />
                           </svg>
-                      }
-                    >
-                      Download
-                    </Button>
+                        }
+                      >
+                        Download
+                      </Button>
+                    </div>
                   </li>
                   <div className="border-b mb-6"></div>
                 </>
