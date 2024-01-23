3bb60c1cf1d7509724b308f2e17354f6cddcec45
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-01-23 16:56:19 +0100

    delete: tsconfig and typescript

--- src/app/template.tsx
+++
@@ -1,4 +0,0 @@
-//This is just an example template to remember we can use this functionality
-export default function Template({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
-}
