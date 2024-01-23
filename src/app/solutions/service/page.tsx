e31d57dadfc6ecc91eaf4ca296a0f90517a649ee
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-01-23 12:50:51 +0100

    fix: Remove tsssc page

--- src/app/solutions/service/page.tsx
+++ src/app/solutions/service/page.tsx
@@ -79,42 +79,42 @@ const services = [
     description:
       "Der Full Service ist ein ganzheitliches Angebot, das die komplette Fahrzeuginstandhaltung umfasst. ",
     img: "https://www.stadlerrail.com/service/Icon_TSSSA.jpg",
-    href: "/solutions/service/tsssc",
+    href: "/solutions/service/full-service",
   },
   {
     label: "Ersatzteile",
     description:
       "Der Full Service ist ein ganzheitliches Angebot, das die komplette Fahrzeuginstandhaltung umfasst. ",
     img: "https://www.stadlerrail.com/service/Icon_Spare_Parts.jpg",
-    href: "/solutions/service/spare",
+    href: "/solutions/service/full-service",
   },
   {
     label: "Modernisation",
     description:
       "Der Full Service ist ein ganzheitliches Angebot, das die komplette Fahrzeuginstandhaltung umfasst. ",
     img: "https://www.stadlerrail.com/service/Icon_Modernisierung.jpg",
-    href: "/solutions/service/refit",
+    href: "/solutions/service/full-service",
   },
   {
     label: "Revisionen",
     description:
       "Der Full Service ist ein ganzheitliches Angebot, das die komplette Fahrzeuginstandhaltung umfasst. ",
     img: "https://www.stadlerrail.com/service/Icon_Revision.jpg",
-    href: "/solutions/service/overhaul",
+    href: "/solutions/service/full-service",
   },
   {
     label: "Reparaturen",
     description:
       "Der Full Service ist ein ganzheitliches Angebot, das die komplette Fahrzeuginstandhaltung umfasst. ",
     img: "https://www.stadlerrail.com/service/Icons_Repair.jpg",
-    href: "/solutions/service/repair",
+    href: "/solutions/service/full-service",
   },
   {
     label: "ECM",
     description:
       "Der Full Service ist ein ganzheitliches Angebot, das die komplette Fahrzeuginstandhaltung umfasst. ",
     img: "https://www.stadlerrail.com/service/Icon_ECM.jpg",
-    href: "/solutions/service/ecm",
+    href: "/solutions/service/full-service",
   },
 ];
 export default function Service() {
