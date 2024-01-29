0600fd66608b54ea97316d4fbed7dee5f25b1e98
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-01-29 13:46:18 +0100

    fix: contact form

--- src/components/sections/ContactForm.tsx
+++ src/components/sections/ContactForm.tsx
@@ -14,29 +14,29 @@ export default function ContactForm(props?: any) {
             <H2>Kontaktformular</H2>
             <div className="relative z-0 w-full mb-5 group">
               <input
-                type="password"
-                name="floating_password"
-                id="floating_password"
+                type="text"
+                name="betreff"
+                id="betreff"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
               />
               <label
-                htmlFor="floating_password"
+                htmlFor="betreff"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
-                Betreff*
+                Betreff *
               </label>
             </div>
 
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap mb-5">
               <div className="flex items-center me-4">
                 <input
                   id="red-radio"
                   type="radio"
                   value=""
                   name="colored-radio"
-                  className="w-4 h-4 text-primary bg-gray-100 border-gray-300 focus:ring-primary focus:ring-2"
+                  className=" w-4 h-4 text-primary bg-gray-100 border-gray-300 focus:ring-primary focus:ring-2"
                 />
                 <label
                   htmlFor="red-radio"
@@ -51,7 +51,7 @@ export default function ContactForm(props?: any) {
                   type="radio"
                   value=""
                   name="colored-radio"
-                  className="w-4 h-4 text-primary bg-gray-100 border-gray-300 focus:ring-primary focus:ring-2"
+                  className=" w-4 h-4 text-primary bg-gray-100 border-gray-300 focus:ring-primary focus:ring-2"
                 />
                 <label
                   htmlFor="green-radio"
@@ -76,7 +76,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_first_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Vorname*
+                  Vorname *
                 </label>
               </div>
               <div className="relative z-0 w-full mb-5 group">
@@ -92,7 +92,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_last_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Nachname*
+                  Nachname *
                 </label>
               </div>
             </div>
@@ -110,7 +110,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_first_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Strasse*
+                  Strasse *
                 </label>
               </div>
               <div className="relative z-0 w-full mb-5 group">
@@ -126,7 +126,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_last_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Nr.*
+                  Nr. *
                 </label>
               </div>
             </div>
@@ -144,7 +144,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_first_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  PLZ*
+                  PLZ *
                 </label>
               </div>
               <div className="relative z-0 w-full mb-5 group">
@@ -160,7 +160,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_last_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Ort*
+                  Ort *
                 </label>
               </div>
             </div>
@@ -180,7 +180,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_phone"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Email*
+                  Email *
                 </label>
               </div>
               <div className="relative z-0 w-full mb-5 group">
@@ -196,7 +196,7 @@ export default function ContactForm(props?: any) {
                   htmlFor="floating_company"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  Telefon*
+                  Telefon *
                 </label>
               </div>
             </div>
