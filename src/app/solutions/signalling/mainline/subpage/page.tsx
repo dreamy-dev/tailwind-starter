a4dfe8a02e7f2982bdb3f165f81d75a20f5f5a19
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-01-29 15:55:29 +0100

    fixed: spaces

--- src/app/solutions/signalling/mainline/subpage/page.tsx
+++ src/app/solutions/signalling/mainline/subpage/page.tsx
@@ -76,13 +76,8 @@ export default function SubPage() {
       />
 
       <ImageFullWidth imgSrc="/ETCS-pano.jpg" />
-      <section className="bg-white dark:bg-gray-900 py-24">
-      <SmallWidth>
-        <Text>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </Text>
-      </SmallWidth>
-      </section>
+     
+    <TextSection></TextSection>
       <Advantages items={advantageItems}></Advantages>
       <Contact></Contact>
     </>
