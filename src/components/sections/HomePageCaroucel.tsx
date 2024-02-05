4b014b10f9d0af6c47435e262222657ecfe1e25c
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-05 16:39:15 +0100

    add icons

--- src/components/sections/HomePageCaroucel.tsx
+++ src/components/sections/HomePageCaroucel.tsx
@@ -59,9 +59,6 @@ const HomePageCaroucel: React.FC<CarouselProps> = ({
     };
   }, []);
 
-  /*  swipe logic starts here */
-
-  /*   swipe logic ends here */
 
   const onPrevClick = () => {
     if (current > 0) {
@@ -110,8 +107,9 @@ const HomePageCaroucel: React.FC<CarouselProps> = ({
                       key={idx}
                       className="min-w-[100%] relative lg:min-w-[43%] md:flex-row  testimonial-motion-div shadow-md shadow-greyDarken-300"
                       animate={{
-                        translateX: `calc(-${current * 100}% - ${current *
-                          2}rem)`,
+                        translateX: `calc(-${current * 100}% - ${
+                          current * 2
+                        }rem)`,
 
                         opacity:
                           idx === current || idx === current + 1 ? 1 : 0.3,
@@ -140,25 +138,11 @@ const HomePageCaroucel: React.FC<CarouselProps> = ({
                           href="#"
                           className="absolute bottom-[20px] left-[22px] inline-flex items-center py-2 text-sm font-medium text-center"
                         >
-                          <svg
-                            width="20"
-                            height="20"
-                            viewBox="0 0 20 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <g clipPath="url(#clip0_4995_6662)">
-                              <path
-                                d="M7.72573e-07 11.1628L16.338 11.1628L10.9296 18.6047L12.7324 20L20 10L12.7324 -6.35355e-07L10.9296 1.39535L16.338 8.83721L9.75882e-07 8.83721L7.72573e-07 11.1628Z"
-                                fill="#005893"
-                              />
-                            </g>
-                            <defs>
-                              <clipPath id="clip0_4995_6662">
-                                <rect width="20" height="20" fill="white" />
-                              </clipPath>
-                            </defs>
-                          </svg>
+                          <img
+                            className="w-5 h-5"
+                            src="/icons/ohne-box/arrow_forward_FILL0_wght400_GRAD0_opsz24_blue.svg"
+                            alt=""
+                          />
                         </Link>
                       </div>
                     </TestimonialMotionDiv>
@@ -176,32 +160,18 @@ const HomePageCaroucel: React.FC<CarouselProps> = ({
                 exit={{ opacity: 0 }}
               >
                 <button onClick={onPrevClick}>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M14.8586 20L4 10L14.8586 0L16.4099 1.68288L7.38294 10L16.4099 18.3171L14.8586 20Z"
-                      fill="#005893"
-                    />
-                  </svg>
+                  <img
+                    className="w-5 h-5"
+                    src="/icons/ohne-box/chevron_left_FILL0_wght400_GRAD0_opsz24_blue.svg"
+                    alt=""
+                  />
                 </button>
                 <button onClick={onNextClick}>
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M5.55124 20L4 18.3171L13.0269 10L4 1.68288L5.55124 0L16.4099 10L5.55124 20Z"
-                      fill="#005893"
-                    />
-                  </svg>
+                  <img
+                    className="w-5 h-5"
+                    src="/icons/ohne-box/chevron_right_FILL0_wght400_GRAD0_opsz24_blue.svg"
+                    alt=""
+                  />
                 </button>
               </motion.div>
             </div>
