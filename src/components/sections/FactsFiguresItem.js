0cb83338d527ce396464eb4d154e04b2b8ad5fcc
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-13 13:38:44 +0100

    fix: grid_container according to name in storyblok

--- src/components/sections/FactsFiguresItem.js
+++ src/components/sections/FactsFiguresItem.js
@@ -10,14 +10,14 @@ const FactsAndFiguresItem = ({ blok }) => {
         <dt className="text-xl leading-7 text-black"></dt>
         <dd className="order-first text-6xl font-bold tracking-tight text-primary sm:text-5xl">
           <CountUp
-            // start={blok?.start ?? 0}
-            // end={blok?.end ?? ""}
-            // suffix={blok?.suffix ?? " "}
-            // prefix={blok?.prefix ?? " "}
-            // duration={blok?.duration ?? 2}
-            // separator={blok?.separator ?? " "}
-            // decimals={blok?.decimals ?? 0}
-            // decimal={blok?.decimal ?? "'"}
+            start={blok?.start ?? 0}
+            end={blok?.end ?? ""}
+            suffix={blok?.suffix ?? " "}
+            prefix={blok?.prefix ?? " "}
+            duration={blok?.duration ?? 2}
+            separator={blok?.separator ?? " "}
+            decimals={blok?.decimals ?? 0}
+            decimal={blok?.decimal ?? "'"}
             enableScrollSpy
           ></CountUp>
         </dd>
