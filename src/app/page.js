af4411484b388732fafbca1358616cd0156b029f
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-15 09:40:21 +0100

    add dynamic slug in url

--- src/app/page.js
+++ src/app/page.js
@@ -14,10 +14,11 @@ export default async function Home() {
 }
 
 export async function fetchData() {
+  let slug = "blok-tests";
   let sbParams = { version: "public" };
 
   const storyblokApi = getStoryblokApi();
-  return await storyblokApi.get(`cdn/stories/blok-tests`, sbParams, {
+  return await storyblokApi.get(`cdn/stories/${slug}`, sbParams, {
     cache: "no-store",
   });
 }
