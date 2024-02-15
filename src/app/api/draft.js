39c76cbbb9c8e0d041ee87475f58f2e27b143e7d
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-15 09:19:48 +0100

    add dynamic slug in url

--- src/app/api/draft.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  res.setDraftMode({ enable: true });
-  res.end("Draft mode is enabled");
-}
--- src/app/page.js
+++ src/app/page.js
@@ -21,43 +21,25 @@
 //     cache: "no-store",
 //   });
 // }
-
-
-import {
-  useStoryblokState,
-  getStoryblokApi,
-  StoryblokComponent,
-} from "@storyblok/react";
-import Layout from "../components/Layout";
-
-export default function Home({ story }) {
-  story = useStoryblokState(story);
-
+import { getStoryblokApi } from "@storyblok/react/rsc";
+import StoryblokStory from "@storyblok/react/story";
+export default async function Home() {
+  const { data } = await fetchData();
   return (
     <div>
-     
-      <Layout>
-        <StoryblokComponent blok={story.content} />
-      </Layout>
+      <StoryblokStory story={data.story} />
     </div>
   );
 }
 
-export async function getStaticProps() {
-  let slug = "blok-tests";
-  let sbParams = {
-    version: "published", // or 'published'
-  };
+export async function fetchData() {
+let slug = "blok-tests";
+  let sbParams = { version: "published" };
 
   const storyblokApi = getStoryblokApi();
-  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
-
-  return {
-    props: {
-      story: data ? data.story : false,
-      key: data ? data.story.id : false,
-    },
-    revalidate: 3600,
-  };
+  return await storyblokApi.get(`cdn/stories/${slug}`, sbParams, {
+    cache: "no-store",
+  });
 }
+
 
