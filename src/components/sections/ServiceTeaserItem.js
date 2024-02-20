8a6361dd984168032d19cc36c00b4bdfbbc9c57c
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-20 13:56:27 +0100

    add link

--- src/components/sections/ServiceTeaserItem.js
+++ src/components/sections/ServiceTeaserItem.js
@@ -8,20 +8,17 @@ imageStyles,
 blok
 }) => {
   return (
-    <div
-     
-      {...storyblokEditable(blok)}
-    >
-      {/* <a href={item.href}> */}
-      <img className="w-24 pl-5 pt-5" src={blok?.Icon.filename} alt="" />
+    <div {...storyblokEditable(blok)}>
+      <a href={blok.link.cached_url}>
+        <img className="w-24 pl-5 pt-5" src={blok?.Icon.filename} alt="" />
 
-      <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {blok?.title}
-        </h5>
-        <p>{blok?.text}</p>
-      </div>
-      {/* </a> */}
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {blok?.title}
+          </h5>
+          <p>{blok?.text}</p>
+        </div>
+      </a>
     </div>
   );
 };
