a048a44e73faad2c51a7a43814d9cd1a0a39be3c
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-20 10:02:38 +0100

    fix: property of image

--- src/components/sections/AdvantageItem.js
+++ src/components/sections/AdvantageItem.js
@@ -7,8 +7,8 @@ const AdvantageItem = ({ blok }) => (
     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primaryTrans-100 lg:h-12 lg:w-12 dark:bg-primary-900">
       {blok?.icon.filename}
     </div>
-    <H4>{blok?.title}</H4>
-    <p className="text-gray-500 dark:text-gray-400">{blok?.text}</p>
+    <H4>{blok?.Title}</H4>
+    <p className="text-gray-500 dark:text-gray-400">{blok?.Text}</p>
   </div>
 );
 
