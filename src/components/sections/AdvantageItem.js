2470b9e23da1bd378d2b5b98d58de6bda39666bd
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-20 09:52:28 +0100

    fix: property of image

--- src/components/sections/AdvantageItem.js
+++ src/components/sections/AdvantageItem.js
@@ -5,7 +5,7 @@ import H4 from "../typography/H4";
 const AdvantageItem = ({ blok }) => (
   <div {...storyblokEditable(blok)}>
     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primaryTrans-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-      {blok?.image.filename}
+      {blok?.icon.filename}
     </div>
     <H4>{blok?.title}</H4>
     <p className="text-gray-500 dark:text-gray-400">{blok?.text}</p>
