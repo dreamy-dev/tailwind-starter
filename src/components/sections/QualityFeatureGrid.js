ec30f7998fd04acf5ba71d3a84c400d10285ea2f
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-23 08:58:37 +0100

    fix errors

--- src/components/sections/QualityFeatureGrid.js
+++ src/components/sections/QualityFeatureGrid.js
@@ -10,9 +10,15 @@ const QualityFeatureGrid = ({ blok }) => {
           <div className="col-span-12 max-w-full mx-auto">
             <H2 styles="text-white">Lorem ipsum</H2>
             <div className="mt-8 lg:mt-16">
-              <div className="grid grid-cols-1 gap-16 sm:gap-12 lg:grid-cols-2 lg:gap-24" {...storyblokEditable(blok)}>
-                {blok.columns.map((nestedBlok) => (
-                  <StoryblokComponent blok={nestedBlok} key={nestedBlok?._uid} />
+              <div
+                className="grid grid-cols-1 gap-16 sm:gap-12 lg:grid-cols-2 lg:gap-24"
+                {...storyblokEditable(blok)}
+              >
+                {blok.feature_grid.map((nestedBlok) => (
+                  <StoryblokComponent
+                    blok={nestedBlok}
+                    key={nestedBlok?._uid}
+                  />
                 ))}
               </div>
             </div>
