6b0a484a1e61c3c053ece52d4aa37fd2e3653167
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-23 15:20:02 +0100

    contact testing

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -8,6 +8,7 @@ async function fetchData(slug) {
     resolve_links: "url",
     version: "published",
     cv: isDev || isDraft ? Date.now() : undefined,
+    resolve_relations: "global_reference.reference",
   };
 
   const storyblokApi = getStoryblokApi();
@@ -58,63 +59,3 @@ export default async function Home({ params }) {
   );
 }
 
-
-
-// const isDev = 'development'
-// export const revalidate = isDev ? 0 : 3600
-
-
-// async function fetchData(slug) {
- 
-//   const sbParams = {
-//     resolve_links: 'url',
-//     version: "public",
-   
-//   };
-
-//   const storyblokApi = getStoryblokApi()
-
-//   return storyblokApi.get(`cdn/stories/${slug}`, sbParams
-//   )
-  
-// }
-
-// export async function generateMetadata({ params }) {
-//   const slug = params?.slug ? params.slug.join("/") : "blok-tests";
-//   const { story } = await fetchData(slug);
-
-//   if (!story) {
-//     return {};
-//   }
-
-//   const title = story.content?.seo?.title || story.name;
-//   const description = story.content?.seo?.description;
-//   return {
-//     metadataBase: new URL("https://stadler.ch"),
-//     title: `${title} · Stadler`,
-//     description: description,
-//     robots: {
-//       index: true,
-//       follow: true,
-//     },
-//     openGraph: {
-//       title: title,
-//       description: description,
-//       url: `/${story.slug}`,
-//     },
-//   };
-//}
-
-// export default async function Home({ params }) {
-//   const slug = params?.slug ? params.slug.join("/") : "blok-tests";
-
-//   const { data } = await fetchData(slug)
-
-//   return (
-//     <>
-//       <StoryblokStory
-//         story={data.story}
-//       />
-//     </>
-//   );
-// }
