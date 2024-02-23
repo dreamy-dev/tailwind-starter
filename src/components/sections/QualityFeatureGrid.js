9778c4922eea44710819d2a25dbdd0840c5cc4f5
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-23 11:30:14 +0100

    fixes

--- src/components/sections/QualityFeatureGrid.js
+++ src/components/sections/QualityFeatureGrid.js
@@ -7,7 +7,7 @@ const QualityFeatureGrid = ({ blok }) => {
     return (
       <section className="py-24 bg-stadlergradient">
         <ContentWidth>
-          <div className="col-span-12 max-w-full mx-auto">
+          <div className="col-span-12 max-w-full">
             <H2 styles="text-white item-start">{ blok?.title}</H2>
             <div className="mt-8 lg:mt-16">
               <div className="grid grid-cols-1 gap-16 sm:gap-12 lg:grid-cols-2 lg:gap-24" {...storyblokEditable(blok)}>
