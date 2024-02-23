ee6764c66b98e0f2c46861f9fb998588443b2059
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-23 14:39:22 +0100

    contact testing

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -1,5 +1,6 @@
-import { ISbStoriesParams, getStoryblokApi, StoryblokStory } from '@storyblok/react/rsc'
-import { Metadata } from "next";
+import { getStoryblokApi, StoryblokStory } from '@storyblok/react/rsc'
+import { resolve } from 'styled-jsx/css'
+
 
 
 const isDev = 'development'
@@ -10,40 +11,41 @@ async function fetchData(slug) {
  
   const sbParams = {
     //resolve_links: 'url',
-    version: 'public',
-  
-  }
+    version: "public",
+    resolve_relations: ["global-contact-section"],
+  };
 
   const storyblokApi = getStoryblokApi()
 
-  return storyblokApi.get(`cdn/stories/${slug}`, sbParams)
+  return storyblokApi.get(`cdn/stories/${slug}`, sbParams
+ )
 }
 
-export async function generateMetadata({ params }) {
-  const slug = params?.slug ? params.slug.join("/") : "home";
-  const { story } = await fetchData(slug);
+// export async function generateMetadata({ params }) {
+//   const slug = params?.slug ? params.slug.join("/") : "blok-tests";
+//   const { story } = await fetchData(slug);
 
-  if (!story) {
-    return {};
-  }
+//   if (!story) {
+//     return {};
+//   }
 
-  const title = story.content?.seo?.title || story.name;
-  const description = story.content?.seo?.description;
-  return {
-    metadataBase: new URL("https://stadler.ch"),
-    title: `${title} · Stadler`,
-    description: description,
-    robots: {
-      index: true,
-      follow: true,
-    },
-    openGraph: {
-      title: title,
-      description: description,
-      url: `/${story.slug}`,
-    },
-  };
-}
+//   const title = story.content?.seo?.title || story.name;
+//   const description = story.content?.seo?.description;
+//   return {
+//     metadataBase: new URL("https://stadler.ch"),
+//     title: `${title} · Stadler`,
+//     description: description,
+//     robots: {
+//       index: true,
+//       follow: true,
+//     },
+//     openGraph: {
+//       title: title,
+//       description: description,
+//       url: `/${story.slug}`,
+//     },
+//   };
+//}
 
 export default async function Home({ params }) {
   const slug = params?.slug ? params.slug.join("/") : "blok-tests";
@@ -52,9 +54,10 @@ export default async function Home({ params }) {
 
   return (
     <>
-   
-      <StoryblokStory story={data.story} />
-
+      <StoryblokStory
+        story={data.story}
+        bridgeOptions={{ resolveRelations: ["global-contact-section"] }}
+      />
     </>
-  )
+  );
 }
