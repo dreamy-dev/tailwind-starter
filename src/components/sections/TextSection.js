1a8e52824cc95c95167d96812280f5785983873b
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-26 11:45:25 +0100

    add: trippleimage

--- src/components/sections/TextSection.js
+++ src/components/sections/TextSection.js
@@ -1,25 +1,16 @@
 
-import { storyblokEditable } from "@storyblok/react/rsc";import SmallWidth from "../../components/layouts/SmallWidth";
+import { storyblokEditable } from "@storyblok/react/rsc";
+import SmallWidth from "../../components/layouts/SmallWidth";
 import Text from "../typography/Text";
 
-export default function TextSection(props, children, blok) {
+export default function TextSection(blok) {
   return (
     <section
       {...storyblokEditable(blok)}
       className="bg-white dark:bg-gray-900 py-24"
     >
       <SmallWidth>
-        <Text>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </Text>
+        <Text>{blok?.text.content.map((item) => item.content[0].text)}</Text>
       </SmallWidth>
     </section>
   );
