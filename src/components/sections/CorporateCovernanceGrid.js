52cf383efa1830c503166b1cabe9a17c658b70ca
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-02-29 08:35:47 +0100

    fix:imports

--- src/components/sections/CorporateCovernanceGrid.js
+++ src/components/sections/CorporateCovernanceGrid.js
@@ -18,7 +18,7 @@ const CorporateGorvernanceGrid = ({ blok}) => {
             </Text>
           </div>
           <div
-            className={`grid gap-8 sm:grid-cols-2 lg:grid-cols-${numberOfColumns}`}
+            className="grid gap-8 sm:grid-cols-2 lg:grid-cols-2"
             {...storyblokEditable(blok)}  >
              {blok?.teaserCards.map((nestedBlok) => (
                   <StoryblokComponent
@@ -26,42 +26,7 @@ const CorporateGorvernanceGrid = ({ blok}) => {
                     key={nestedBlok?._uid}
                   />
                 ))}
-            {/* <a href="unternehmen/verwaltungsrat">
-              <article className="p-4 h-full flex flex-col flex-wrap justify-between content-start bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <h2 className="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  Verwaltungsrat
-                </h2>
-                <p className="mb-4 font-normal text-black dark:text-gray-400">
-                  Erfahren Sie mehr über den Verwaltungsrat von Stadler.
-                </p>
-                <img className="w-5" src="/icons/arrow-blue.svg" alt="" />
-              </article>
-            </a>
-            <a href="/unternehmen/konzernleitung">
-              <article className="p-4 h-full flex flex-col flex-wrap justify-between content-start bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <h2 className="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  Konzernleitung
-                </h2>
-                <p className="mb-4 font-normal text-black dark:text-gray-400">
-                  Erfahren Sie mehr über die Konzernleitung von Stadler.
-                </p>
-                <img className="w-5" src="/icons/arrow-blue.svg" alt="" />
-              </article>
-            </a>
-            {showThirdCard && (
-              <a href="/compliance">
-                <article className="p-4 h-full flex flex-col flex-wrap justify-between content-start bg-white  border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                  <h2 className="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Compliance
-                  </h2>
-                  <p className="mb-4 font-normal text-black dark:text-gray-400">
-                    Erfahren Sie, wie Stadler sicherstellt, dass juristische und
-                    ethische Regeln eingehalten werden.
-                  </p>
-                  <img className="w-5" src="/icons/arrow-blue.svg" alt="" />
-                </article>
-              </a>
-            )} */}
+        
           </div>
         </div>
       </SuperSmallWidth>
