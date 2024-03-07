63fed0785905b740daceb9d6b40ecb8ebb33f7d8
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-03-07 19:51:46 +0100

    feature: news update + breadcrumbs

---
+++ src/components/sections/Breadcrumbs.js
@@ -0,0 +1,64 @@
+import  { useEffect, useState } from 'react';
+import { useRouter, usePathname } from 'next/navigation';
+
+const convertBreadcrumb = string => {
+    string
+     .replace(/-/g, ' ')
+     .replace(/oe/g, 'ö')
+     .replace(/ae/g, 'ä')
+     .replace(/ue/g, 'ü')
+ 
+     return string.charAt(0).toUpperCase() + string.slice(1);
+ };
+ 
+ const Breadcrumbs = () => {
+   const router = useRouter();
+   const path = usePathname();
+   const [breadcrumbs, setBreadcrumbs] = useState(null);
+ 
+   useEffect(() => {
+     if (router) {
+       const linkPath = path?.split('/');
+       linkPath.shift();
+       linkPath.splice(-1)
+ 
+       const pathArray = linkPath.map((path, i) => {
+         return { breadcrumb: path, href: '/' + linkPath.slice(0, i + 1).join('/') };
+       });
+ 
+       setBreadcrumbs(pathArray);
+     }
+   }, [router]);
+ 
+   if (!breadcrumbs) {
+     return null;
+   }
+ 
+   return (
+     <nav>
+       <ol className="mt-20 flex font-normal text-gray-600">
+         {breadcrumbs.map((breadcrumb, i) => {
+           return (
+             <li key={i}>
+               {i !== breadcrumbs.length - 1 ? (
+               <>
+                 <a href={breadcrumb.href}>
+                   {convertBreadcrumb(breadcrumb.breadcrumb)}
+                 </a>
+                 <span className="px-3">|</span>
+               </>
+             ) : (
+               <a href={breadcrumb.href}>
+               {convertBreadcrumb(breadcrumb.breadcrumb)}
+             </a>
+               )}
+             </li>
+           );
+         })}
+       </ol>
+     </nav>
+   );
+ };
+
+ export default Breadcrumbs;
+ 
--- src/components/sections/NewsBody.js
+++ src/components/sections/NewsBody.js
@@ -1,24 +1,47 @@
-import { storyblokEditable } from "@storyblok/react/rsc";
 
-import ContentWidth from "../layouts/ContentWidth";
+import { storyblokEditable } from "@storyblok/react/rsc";
+import { render } from 'storyblok-rich-text-react-renderer';
+
+import SuperSmallWidth from "../layouts/SuperSmallWidth";
+import H1 from "../typography/H1";
 import Text from "../typography/Text";
+import Breadcrumbs from "./Breadcrumbs";
 
 function NewsBody({blok}) {
   console.log("block", blok)
   return (
     <section {...storyblokEditable(blok)}>
-      <ContentWidth>
-      <div className="col-span-12 w-full">
-      <Text styles="text-md mb-4">
-            Die EURO9000 ist eine interoperable Lokomotive, die speziell für den
-            Einsatz auf internationalen Strecken entlang der wichtigsten
-            europäischen Bahnkorridore konzipiert wurde. Mit bereits bestehenden
-            Zulassungen in Deutschland, Österreich und der Schweiz hat die
-            Lokomotive nun auch die Zulassung für den Betrieb in Belgien und den
-            Niederlanden erhalten.
-          </Text>
+      <SuperSmallWidth>
+      <div className="mt-20 flex font-normal text-gray-600">
+        <Breadcrumbs />
       </div>
-    </ContentWidth>
+      <H1 styles="mb-8">{blok.title}</H1>
+      <div className="grid grid-cols-12 gap-6 mb-6">
+        <div className="col-span-6">
+        {JSON.stringify(blok?.categories)}
+          {/* {tags?.map((tag, index) => (
+            <span
+              key={index}
+              className="mb-2 inline text-gray-700 px-4 py-2 mr-4 border border-gray-400 text-xs last-of-type:mr-0"
+            >
+              {tag.text}
+            </span>
+          ))} */}
+        </div>
+        <div className="col-span-6 align-right text-right">
+          <Text styles="text-md">{blok.date}</Text>
+        </div>
+      </div>
+      {/* <Text styles="text-lg mt-12">{leadText}</Text> */}
+    </SuperSmallWidth>
+      <SuperSmallWidth>
+      <div className="col-span-12 w-full">
+        <img src={blok?.image[0].filename} className="my-8" alt="full service" />
+        <Text>
+          {render(blok.text)}
+        </Text>
+      </div>
+    </SuperSmallWidth>
     </section>
   );
 }
