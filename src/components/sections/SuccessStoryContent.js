45517f6853ae35a6f9b231fed390afea730cfadf
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-03-19 16:01:02 +0100

    fix: Update succes story content

--- src/components/sections/SuccessStoryContent.js
+++ src/components/sections/SuccessStoryContent.js
@@ -6,32 +6,34 @@ import Text from '../typography/Text';
 import ContentWidth from '../layouts/ContentWidth';
 import H1 from '../typography/H1';
 import { render } from 'storyblok-rich-text-react-renderer';
+import HeroText from './HeroText';
+import SmallWidth from '../layouts/SmallWidth';
+import Lead from '../typography/Lead';
 
 const SuccessStoryContent = ({ blok }) => {
     return (
-        <section
-            {...storyblokEditable(blok)}
-            className="min-w-[100%] relative lg:min-w-[43%] md:flex-row"
-        >
-            <ContentWidth>
-                <div className="col-span-12">
-                    <img
-                        className="object-right max-w-full w-full h-auto"
-                        src={blok?.image.filename}
-                        alt=""
-                    />
-                    <div className="mb-4">
-                        <H1>{blok.title}</H1>
+        <div {...storyblokEditable(blok)}>
+            <section className="bg-white dark:bg-gray-900 py-24">
+                <SmallWidth>
+                    <H1>{blok?.title}</H1>
+                    <Lead className="richtext">{blok?.teaser}</Lead>
+                </SmallWidth>
+            </section>
+            <section className="min-w-[100%] relative lg:min-w-[43%] md:flex-row">
+                <ContentWidth>
+                    <div className="col-span-12">
+                        <img
+                            className="object-right max-w-full w-full h-auto"
+                            src={blok?.image.filename}
+                            alt=""
+                        />
+                        <div className="my-14 richtext">
+                            <Text>{render(blok.text)}</Text>
+                        </div>
                     </div>
-                    <div className="mb-14">
-                        <Text>{blok.teaser}</Text>
-                    </div>
-                    <div className="mb-14 richtext">
-                        <Text>{render(blok.text)}</Text>
-                    </div>
-                </div>
-            </ContentWidth>
-        </section>
+                </ContentWidth>
+            </section>
+        </div>
     );
 };
 
