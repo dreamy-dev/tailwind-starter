c7a7594359874ac2442ab2c1097b622dfab6e1e8
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-04-04 16:37:43 +0200

    feature: subscribe to the email

--- src/components/sections/MailchimpEmbed.js
+++ src/components/sections/MailchimpEmbed.js
@@ -1,7 +1,8 @@
 import { storyblokEditable, StoryblokComponent } from '@storyblok/react/rsc';
 import ButtonPrimary from '../elements/ButtonPrimary';
 import FullWidth from '../layouts/FullWidth';
-import SmallWidth from '../layouts/SmallWidth';
+import ContentWidth from '../layouts/ContentWidth';
+import Text from '../typography/Text';
 import H2 from '../typography/H2';
 import Script from 'next/script';
 import { useState, useEffect } from 'react';
@@ -13,45 +14,50 @@ export default function MailchimpEmbed({ blok }) {
     const [company, setCompany] = useState('');
     return (
         <section {...storyblokEditable(blok)} className="bg-white my-20 ">
-            <div className="px-4 mx-auto max-w-screen-xl lg:px-6">
-                <div className="gap-16 justify-between items-center p-6 text-white bg-primary lg:flex lg:gap-24 md:p-12">
-
-                    <H2 styles="mb-4">Sign up for our newsletter</H2>
-                    <p className="font-light text-white sm:text-xl">
-                        Stay up to date with the roadmap progress,
-                        announcements and exclusive discounts feel free to
-                        sign up with your email.
-                    </p>
-                    <div id="mc_embed_shell">
-                        <div id="mc_embed_signup" className="mt-6 w-full lg:mt-0">
+            <ContentWidth>
+                <div className="col-span-12">
+                    <H2 styles="mb-4">{blok.title}</H2>
+                    <Text className="font-light text-white sm:text-xl">
+                        {blok.description}
+                    </Text>
+                    <div id="mc_embed_shell" className="col-span-12 mt-8 w-full">
+                        <div id="mc_embed_signup" className="w-full">
                             <form action="https://stadlerrail.us14.list-manage.com/subscribe/post?u=7e29f428e402a1b5e2a54d074&amp;id=abef8b12b4&amp;f_id=00b922e3f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
-                                <div id="mc_embed_signup_scroll">
-                                    <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
-                                    <div className="mc-field-group">
-                                        <label for="mce-EMAIL" className="mb-2 text-sm font-medium text-white">Email <span className="asterisk">*</span></label>
-                                        <input type="email" name="EMAIL" value={email} onChange={e => setEmail(e.target.value)} className="required email block p-3 w-full text-sm text-gray-900 bg-gray-50  border border-white" id="mce-EMAIL" required="" />
-                                    </div>
-                                    <div className="items-center mb-8 space-y-4 flex-col">
+                                <div id="mc_embed_signup_scroll" className="items-center space-y-4 flex-col">
+                                    {/* <div className="indicates-required"><span className="asterisk">*</span> indicates required</div> */}
+                                    <div className="grid grid-cols-1 gap-8 lg:gap-4 lg:grid-cols-2 items-center">
                                         <div className="mc-field-group relative w-full">
-                                            <label for="mce-FNAME" className="mb-2 text-sm font-medium text-white">First Name <span className="asterisk">*</span></label>
-                                            <input type="text" name="FNAME" value={firstName} onChange={e => setFirstName(e.target.value)} className="required text block p-3 w-full text-sm text-gray-900 bg-gray-50  border border-white" id="mce-FNAME" required="" />
+                                            <label for="mce-FNAME" className="mb-2 text-sm font-medium">{blok.fname_label} <span className="asterisk">*</span></label>
+                                            <input type="text" name="FNAME" value={firstName} onChange={e => setFirstName(e.target.value)} className="required text block p-3  w-full text-sm text-gray-900 border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" id="mce-FNAME" required="" />
                                         </div>
                                         <div className="mc-field-group relative w-full">
-                                            <label for="mce-LNAME" className="mb-2 text-sm font-medium text-white">Last Name <span className="asterisk">*</span></label>
-                                            <input type="text" name="LNAME" value={lastName} onChange={e => setLastName(e.target.value)} className="required text block p-3 w-full text-sm text-gray-900 bg-gray-50  border border-white" id="mce-LNAME" required="" />
+                                            <label for="mce-LNAME" className="mb-2 text-sm font-medium">{blok.lname_label} <span className="asterisk">*</span></label>
+                                            <input type="text" name="LNAME" value={lastName} onChange={e => setLastName(e.target.value)} className="required text block p-3  w-full text-sm text-gray-900 border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" id="mce-LNAME" required="" />
                                         </div>
                                     </div>
                                     <div className="mc-field-group">
-                                        <label for="mce-COMPANY" className="mb-2 text-sm font-medium text-white">Company <span className="asterisk">*</span></label>
-                                        <input type="text" name="COMPANY" value={company} onChange={e => setCompany(e.target.value)} className="required text block p-3 w-full text-sm text-gray-900 bg-gray-50 border border-white" id="mce-COMPANY" required="" />
+                                        <label for="mce-EMAIL" className="mb-2 text-sm font-medium">{blok.email_label} <span className="asterisk">*</span></label>
+                                        <input type="email" name="EMAIL" value={email} onChange={e => setEmail(e.target.value)} className="required email block p-3  w-full text-sm text-gray-900 border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" id="mce-EMAIL" required="" />
                                     </div>
-                                    <div id="mce-responses" className="clear">
+                                    <div className="mc-field-group">
+                                        <label for="mce-COMPANY" className="mb-2 text-sm font-medium">{blok.company_label} <span className="asterisk">*</span></label>
+                                        <input type="text" name="COMPANY" value={company} onChange={e => setCompany(e.target.value)} className="required text block p-3  w-full text-sm text-gray-900 border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" id="mce-COMPANY" required="" />
+                                    </div>
+                                    <div class="mc-field-group input-group">
+                                        <ul>
+                                            <li className="flex items-start">
+                                                <input required="" type="checkbox" className="required relative shrink-0 w-4 h-4 border-2 border-primary checked:bg-primary checked:border-primary" name="MMERGE3" id="mce-MMERGE30" value="First Choice" />
+                                                <label className="ms-2 text-sm text-gray-500 dark:text-gray-300" for="mce-MMERGE30">{blok.checkbox_label} *</label>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div id="mce-responses" className="clear mb-16">
                                         <div className="response hidden" id="mce-error-response"></div>
                                         <div className="response hidden" id="mce-success-response"></div>
                                     </div>
                                     <div className="hidden" aria-hidden="true"><input type="text" name="b_7e29f428e402a1b5e2a54d074_abef8b12b4" tabindex="-1" value="" /></div>
-                                    <div className="clear flex center">
-                                        <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="flex text-sm px-5 py-3 items-center hover:primary-800 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded" value="Subscribe" />
+                                    <div className="clear flex justify-center">
+                                        <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="bg-stadlergradient text-white text-sm px-5 py-2.5 leading-6 font-medium rounded flex items-center gap-2" value={blok.button_text} />
                                     </div>
                                 </div>
                             </form>
@@ -60,7 +66,7 @@ export default function MailchimpEmbed({ blok }) {
                         <Script type="text/javascript">{`(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[6]='COMPANY';ftypes[6]='text';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='MMERGE5';ftypes[5]='address';fnames[7]='MMERGE7';ftypes[7]='dropdown';}(jQuery));var $mcj = jQuery.noConflict(true);`}</Script>
                     </div>
                 </div>
-            </div>
-        </section>
+            </ContentWidth>
+        </section >
     )
 }
