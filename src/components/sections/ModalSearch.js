1b76000b4b9b46de5691e542ca3a3d753f7fa1b7
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-04-08 16:43:37 +0200

    fix: z-indexes overlays in header

--- src/components/sections/ModalSearch.js
+++ src/components/sections/ModalSearch.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { getStoryblokApi } from '@storyblok/react/rsc';
+import ContentWidth from '../layouts/ContentWidth';
 
 const ModalSearch = ({ isModalOpen, closeModal }) => {
     const [articles, setArticles] = useState([]);
@@ -88,83 +89,85 @@ const onSearchChange = (e) => {
     }, [isModalOpen, closeModal]);
 
     return (
-        <div
-            ref={modalRef}
-            className={`shadow-md absolute top-[100%] right-0 px-4 w-[375px] md:w-[50%] transition-transform duration-500 ${
-                isModalOpen
-                    ? 'transform opacity-100 ease-in-out'
-                    : 'transform opacity-0 ease-in-out'
-            }`}
-        >
+    
             <div
-                ref={contentRef}
-                className="relative bg-white shadow dark:bg-gray-700 h-full"
+                ref={modalRef}
+                className={`col-span-12 z-20 shadow-md absolute top-[100%] right-0  w-[100%] sm:w-[450px] md:w-[710px] transition-transform duration-500 ${
+                    isModalOpen
+                        ? 'transform opacity-100 ease-in-out'
+                        : 'transform opacity-0 ease-in-out'
+                }`}
             >
-                <div className="flex items-center justify-between p-4 md:p-5 border-b dark:border-gray-600">
-                    <div className="pr-2">
-                        <img
-                            className="w-5 h-5"
-                            src="/ohne-box/search_FILL0_wght400_GRAD0_opsz24_blue.svg"
-                            alt=""
-                        />
-                    </div>
-                    <label htmlFor="search" className="w-full">
-                        <input
-                            placeholder="Enter your search term..."
-                            name="search"
-                            onChange={onSearchChange}
-                            value={search}
-                            type="search"
-                            style={{
-                                WebkitAppearance: 'none',
-                                appearance: 'none',
-                            }}
-                            className="p-2 w-full border-primary focus:border-primary appearance-none"
-                        />
-                    </label>
-                    <button
-                        onClick={closeModal}
-                        type="button"
-                        className="text-primary bg-transparent text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-hide="default-modal"
-                    >
-                        <svg
-                            className="w-3 h-3"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="#005893"
-                            viewBox="0 0 14 14"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                <div
+                    ref={contentRef}
+                    className="relative bg-white shadow dark:bg-gray-700 h-full"
+                >
+                    <div className="flex items-center justify-between p-4 md:p-5 border-b dark:border-gray-600">
+                        <div className="pr-2">
+                            <img
+                                className="w-5 h-5"
+                                src="/ohne-box/search_FILL0_wght400_GRAD0_opsz24_blue.svg"
+                                alt=""
                             />
-                        </svg>
-                        <span className="sr-only">Close modal</span>
-                    </button>
-                </div>
+                        </div>
+                        <label htmlFor="search" className="w-full">
+                            <input
+                                placeholder="Enter your search term..."
+                                name="search"
+                                onChange={onSearchChange}
+                                value={search}
+                                type="search"
+                                style={{
+                                    WebkitAppearance: 'none',
+                                    appearance: 'none',
+                                }}
+                                className="p-2 w-full border-primary focus:border-primary appearance-none"
+                            />
+                        </label>
+                        <button
+                            onClick={closeModal}
+                            type="button"
+                            className="text-primary bg-transparent text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            data-modal-hide="default-modal"
+                        >
+                            <svg
+                                className="w-3 h-3"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#005893"
+                                viewBox="0 0 14 14"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                />
+                            </svg>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
 
-                <div className="p-2 md:p-3 overflow-y-scroll h-[385px]">
-                    <div className="flex flex-col">
-                        {articles[0] &&
-                            articles.map((article) => (
-                                <a
-                                    href={`/${article.full_slug}`}
-                                    className="group mb-6 transition-all"
-                                    key={article.uuid}
-                                >
-                                    <h2 className="mb-1 text-base font-normal leading-tight text-gray-900 group-hover:text-primary transition-all">
-                                        {article.name}
-                                    </h2>
-                                </a>
-                            ))}
+                    <div className="p-2 md:p-3 overflow-y-scroll h-[385px]">
+                        <div className="flex flex-col">
+                            {articles[0] &&
+                                articles.map((article) => (
+                                    <a
+                                        href={`/${article.full_slug}`}
+                                        className="group mb-6 transition-all"
+                                        key={article.uuid}
+                                    >
+                                        <h2 className="mb-1 text-base font-normal leading-tight text-gray-900 group-hover:text-primary transition-all">
+                                            {article.name}
+                                        </h2>
+                                    </a>
+                                ))}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+    
     );
 };
 
