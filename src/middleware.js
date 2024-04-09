bb548cf112a0bb9742342557ca5e8435c2d2bb55
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-04-09 14:21:50 +0200

    🌐 Install vercel and test middleware request

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,14 @@
 import { i18nRouter } from 'next-i18n-router';
 import i18nConfig from '../i18nConfig';
+import { NextResponse } from 'next/server';
 
+// Middleware used for i18n Routing
 export function middleware(request) {
+    const country = (request.geo && request.geo.country) || 'global';
+
+    console.log(`Visitor from ${country}`);
+    console.log(request.geo.country);
+
     return i18nRouter(request, i18nConfig);
 }
 
@@ -9,3 +16,22 @@ export function middleware(request) {
 export const config = {
     matcher: '/((?!api|static|.*\\..*|_next).*)',
 };
+
+// Geolocation
+
+/* export default function middleware(request) {
+    // Extract country. Default to US if not found.
+    const country = (request.geo && request.geo.country) || 'US';
+
+    console.log(`Visitor from ${country}`);
+
+    // Specify the correct route based on the requests location
+    if (country === BLOCKED_COUNTRY) {
+        request.url.pathname = '/login';
+    } else {
+        request.url.pathname = `/secret-page`;
+    }
+
+    // Rewrite to URL
+    return NextResponse.rewrite(request.url);
+} */
