3131657cb63f40045dd7a6c0b3daccd6c53e32b2
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-04-09 17:02:59 +0200

    🌐 Testing geolocation request handler [test]

--- src/middleware.js
+++ src/middleware.js
@@ -8,6 +8,65 @@ export function middleware(request) {
 
     console.log(`Visitor from ${country}`);
     console.log(request.geo.country);
+    console.log(`Pathname ${request.url.pathname}`);
+
+    if ((request.url.pathname == '/de/career') | 'en/career') {
+        switch (request.url.pathname) {
+            case 'CH':
+                request.nextUrl.pathname = '/ch/career-ch-en';
+                return NextResponse.rewrite(request.nextUrl);
+            case 'DE':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'US':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'ES':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'PL':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'HU':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'BY':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'KZ':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'CZ':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'AT':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'GB':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'IT':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'NL':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'SE':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            case 'NO':
+                // request.nextUrl.pathname = 'ch/...'
+                break;
+            default:
+            // bring on global page
+            // current understanding of logic is:
+            // don't bring to the global folder, it would be easier to not do any redirect
+            // and update this career page just in english and german
+            // otherwise redirect here to global folder
+        }
+    } else {
+        // nothing should happen really, right? not sure...
+    }
 
     return i18nRouter(request, i18nConfig);
 }
