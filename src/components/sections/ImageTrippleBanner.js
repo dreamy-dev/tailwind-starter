34c81544905fcea9c46870b7f5370033849b2432
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2024-04-11 10:29:53 +0200

    fix: padding bottom

--- src/components/sections/ImageTrippleBanner.js
+++ src/components/sections/ImageTrippleBanner.js
@@ -5,9 +5,9 @@ import { storyblokEditable } from '@storyblok/react/rsc';
 const ImageTrippleBanner = ({ blok }) => {
     return (
         <section {...storyblokEditable(blok)}>
-            <div className="gap-4 py-0 lg:py-24 sm:grid sm:grid-cols-4 sm:mt-12">
+            <div className="gap-4  sm:grid sm:grid-cols-4 sm:mt-12">
                 <img
-                    className="object-cover h-80 w-full scale-100 col-span-2 mb-4 sm:block  "
+                    className="object-cover h-80 w-full scale-100 col-span-2  sm:block  "
                     src={blok?.image1.filename}
                     alt="content gallery 1"
                 />
