3d2adb9134ca101e2d190b8749e030d2419f5c74
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-04-11 10:07:19 +0200

    ♻️ Clean up middleware file

--- src/middleware.js
+++ src/middleware.js
@@ -4,12 +4,13 @@ import { NextResponse } from 'next/server';
 
 // Middleware used for i18n Routing
 export function middleware(request) {
+    //check if country is one of the defined countries or global
     const country = (request.geo && request.geo.country) || 'global';
 
+    // Console Log values
     console.log(`Visitor from ${country}`);
     console.log(request.geo.country);
     console.log(request);
-    console.log(`URL ${request.url}`);
     console.log(`Pathname ${request.nextUrl.pathname}`);
 
     if (request.nextUrl.pathname == '/de/career') {
@@ -78,22 +79,3 @@ export function middleware(request) {
 export const config = {
     matcher: '/((?!api|static|.*\\..*|_next).*)',
 };
-
-// Geolocation
-
-/* export default function middleware(request) {
-    // Extract country. Default to US if not found.
-    const country = (request.geo && request.geo.country) || 'US';
-
-    console.log(`Visitor from ${country}`);
-
-    // Specify the correct route based on the requests location
-    if (country === BLOCKED_COUNTRY) {
-        request.url.pathname = '/login';
-    } else {
-        request.url.pathname = `/secret-page`;
-    }
-
-    // Rewrite to URL
-    return NextResponse.rewrite(request.url);
-} */
