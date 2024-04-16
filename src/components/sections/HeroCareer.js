4d9ba6970bee95c3fb1988b244660b9e6c5a6581
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-04-16 17:28:29 +0200

    📱 Add responsive styles to career hero

--- src/components/sections/HeroCareer.js
+++ src/components/sections/HeroCareer.js
@@ -4,6 +4,8 @@ import ButtonPrimary from '../../components/elements/ButtonPrimary';
 import H1 from '../../components/typography/H1';
 import Text from '../../components/typography/Text';
 import SmallWidth from '../layouts/SmallWidth';
+import { PinIcon } from '../icons/PinIcon';
+import { CalenderIcon } from '../icons/CalenderIcon';
 
 const HeroCareer = ({ blok }) => {
     return (
@@ -12,13 +14,11 @@ const HeroCareer = ({ blok }) => {
             style={{
                 backgroundImage: `url(${blok?.background_image.filename})`,
             }}
-            className="bg-no-repeat bg-cover bg-left bg-gray-400 bg-blend-multiply"
+            className="bg-no-repeat bg-cover bg-center bg-gray-400 bg-blend-multiply"
         >
             <SmallWidth>
-                <div className="py-16 lg:py-24 col-span-12 relative px-4 mx-auto max-w-screen-xl text-white lg:pt-64 lg:pb-12 xl:px-0 z-1">
+                <div className="py-16 lg:py-24 col-span-12 relative mx-auto max-w-screen-xl text-white lg:pt-80 lg:pb-12 xl:px-0 z-1">
                     <div className="mb-6 max-w-screen-md lg:mb-0 text-white">
-                        <br />
-                        <br />
                         <H1>{blok?.title}</H1>
                         <div className="my-8 lg:mb-8">
                             <Text>{blok?.subheading}</Text>
@@ -26,63 +26,45 @@ const HeroCareer = ({ blok }) => {
                     </div>
                     <form
                         action="#"
-                        className="grid gap-y-4 p-4 mt-8 w-full bg-white lg:gap-x-4 lg:grid-cols-5 lg:mt-9 dark:bg-gray-800"
+                        className="flex justify-between flex-col gap-4 lg:flex-row p-4 mt-16 mb-5 w-full bg-white lg:gap-x-4 lg:grid-cols-5 lg:mt-9 dark:bg-gray-800"
                     >
-                        <div className="grid grid-cols-1 gap-x-4 lg:col-span-2">
+                        <div className="flex-grow gap-x-4 ">
                             <div className="relative">
                                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                    <svg
-                                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                            clipRule="evenodd"
-                                        ></path>
-                                    </svg>
+                                    <CalenderIcon
+                                        color="#00000080"
+                                        styles="w-5 h-5"
+                                    />
                                 </div>
                                 <input
                                     name="end"
                                     type="text"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    className="bg-gray-50 border rounded border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                     placeholder="Berufsfeld"
                                 />
                             </div>
                         </div>
-                        <div className="lg:col-span-2">
+                        <div className="flex-grow">
                             <label className="sr-only">Location</label>
                             <div className="relative">
                                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                    <svg
-                                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                            clipRule="evenodd"
-                                        ></path>
-                                    </svg>
+                                    <PinIcon
+                                        color="#00000080"
+                                        styles="w-5 h-5"
+                                    />
                                 </div>
                                 <input
                                     type="text"
                                     id="location-form"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    className="bg-gray-50 border rounded border-gray-300 text-gray-900 text-sm  focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                     placeholder="Land"
                                 />
                             </div>
                         </div>
-                        <div className="lg:col-span-1 grid content-stretch">
+                        <div className="">
                             <ButtonPrimary buttonText="Suchen" />
                         </div>
                     </form>
-                    <br />
-                    <br />
                 </div>
             </SmallWidth>
         </section>
