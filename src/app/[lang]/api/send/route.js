fc24e5511281b86227ef6823f587bbfe2b7a5541
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-04-18 11:17:55 +0200

    ✨ Send mail after contactform submit [test]

--- src/app/[lang]/api/send/route.js
+++ src/app/[lang]/api/send/route.js
@@ -3,14 +3,38 @@ import { EmailTemplate } from '@/src/components/email-template/EmailTemplate';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(request) {
+    const formData = await request.formData();
+    const name = formData.get('first_name');
+    const lastName = formData.get('last_name');
+    const subject = formData.get('subject');
+    const street = formData.get('street');
+    const number = formData.get('number');
+    const zip = formData.get('zip');
+    const city = formData.get('city');
+    const email = formData.get('email');
+    const phone = formData.get('phone');
+    const message = formData.get('message');
+
     try {
         const data = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: ['cecile.hefti@gateb.com'],
-            subject: 'Contact Page',
-            react: EmailTemplate({ firstName: 'John' }),
+            from: 'GateB Test <info@gateb-test-domain.site>',
+            to: ['cecile.hefti@outlook.com'],
+            subject: 'Hello world',
+            react: EmailTemplate({
+                first_name: name,
+                last_name: lastName,
+                subject: subject,
+                street: street,
+                number: number,
+                zip: zip,
+                city: city,
+                email: email,
+                phone: phone,
+                message: message,
+            }),
         });
+        console.log(data);
 
         return Response.json(data);
     } catch (error) {
--- src/components/email-template/EmailTemplate.js
+++ src/components/email-template/EmailTemplate.js
@@ -1,29 +1,40 @@
 import * as React from 'react';
 
-export const EmailTemplate = ({ firstName }) => (
+export const EmailTemplate = ({
+    subject,
+    first_name,
+    last_name,
+    street,
+    number,
+    zip,
+    city,
+    email,
+    phone,
+    message,
+}) => (
     <div>
         <h1>Neues Kontaktformular</h1>
         <p>Ein neues Kontakformular ist eingegangen. Das sind die Angaben:</p>
-        <p>Betreff:</p>
+        <p>Subject:</p>
         <p>{subject}</p>
-        <p>Herr / Frau</p>
-        <p>Vorname:</p>
-        <p>{firstName}</p>
-        <p>Nachname:</p>
-        <p>{lastName}</p>
-        <p>Strasse:</p>
+        {/* <p>Gender</p> */}
+        <p>First Name:</p>
+        <p>{first_name}</p>
+        <p>Last Name:</p>
+        <p>{last_name}</p>
+        <p>Street:</p>
         <p>{street}</p>
-        <p>Nr:</p>
-        <p>{nr}</p>
-        <p>PLZ:</p>
-        <p>{plz}</p>
-        <p>Ort:</p>
-        <p>{}</p>
+        <p>Number:</p>
+        <p>{number}</p>
+        <p>ZIP:</p>
+        <p>{zip}</p>
+        <p>city:</p>
+        <p>{city}</p>
         <p>E-Mail:</p>
         <p>{email}</p>
-        <p>Telefon:</p>
+        <p>Phone:</p>
         <p>{phone}</p>
-        <p>Bemerkung:</p>
+        <p>Message:</p>
         <p>{message}</p>
     </div>
 );
