f31432ab11a332c018be786ce6dc49d3f01fd52d
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-04-23 21:44:18 +0200

    fix: additional test for mailchimp

--- src/app/[lang]/api/mailchimp-contact/route.js
+++ src/app/[lang]/api/mailchimp-contact/route.js
@@ -6,7 +6,7 @@ export async function POST(request, res) {
     const accessToken = process.env.MAILCHIMP_API_KEY;
     // const url = `https://${process.env.MAILCHIMP_API_SERVER}.api.mailchimp.com/3.0/lists/be8e58f4ee/members`
 
-    const url = `https://${process.env.MAILCHIMP_API_SERVER}.list-manage.com/contact-form/post?u=7e29f428e402a1b5e2a54d074&form_id=2e74855d6562cb5c25c57cd2c93d2fdf`
+    const url = `https://us14.list-manage.com/contact-form/post?u=7e29f428e402a1b5e2a54d074&form_id=2e74855d6562cb5c25c57cd2c93d2fdf`
     try {
         const initialResponse = await fetch(url, {
             headers: {
