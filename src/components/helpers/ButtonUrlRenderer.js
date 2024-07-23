2f8e8cf1dea83376b21b31e266f4c37046b4e1ea
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-07-23 16:56:26 +0200

    fix: link render function

--- src/components/helpers/ButtonUrlRenderer.js
+++ src/components/helpers/ButtonUrlRenderer.js
@@ -1,4 +1,5 @@
 const ButtonUrlRenderer = (data) => {
+    // console.log("ButtonUrlRenderer", data)
     if (!data) {
         return '/';
     }
@@ -6,16 +7,25 @@ const ButtonUrlRenderer = (data) => {
         const anchor = data.anchor ? `#${data.anchor}` : '';
         return `/${data?.story?.full_slug}${anchor}` || '/';
     }
+    if (data.linktype && data.linktype == 'asset') {
+        if (data.url) {
+            return data?.url;
+        }
+        if (data.fieldtype) {
+            return data?.filename;
+        }
+    }
     if (data.linktype && data.linktype == 'url') {
         return data?.url || '/';
     }
-    if (data.linktype && data.fieldtype == 'asset') {
-        return data?.filename;
-    }
-    if (data.fieldtype && data.fieldtype == 'asset') {
-        return data?.filename;
-    }
+    // if (data.linktype && data.fieldtype == 'asset') {
+    //     return data?.filename;
+    // }
+    // if (data.fieldtype && data.fieldtype == 'asset') {
+    //     return data?.filename;
+    // }
     if (data.linktype && data.linktype == 'email') {
+        return data?.url;
     }
     return '/';
 };
