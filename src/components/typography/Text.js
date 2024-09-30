1468b6ce5a3eb7acefcb3075b69f8dbe85d0286a
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-09-30 14:45:49 +0200

    fix: remove background

--- src/components/typography/Text.js
+++ src/components/typography/Text.js
@@ -2,7 +2,6 @@ export default function Text({ children, styles }) {
     return (
         <p
             className={`my-2 text-base font-normal lg:my-3 lg:text-xl ${styles ? styles : ' '}`}
-            style={{ backgroundColor: 'transparent'}}
         >
             {children}
         </p>
