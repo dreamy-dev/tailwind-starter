a1a520fdabadefb1094234bd0a7597eb2f582456
nataliia-karpenko <nataliia.karpenko@gateb.com>
2024-10-01 16:50:12 +0200

    fix: carousel fixes

--- src/components/sections/TrainSequence.js
+++ src/components/sections/TrainSequence.js
@@ -1,5 +1,106 @@
 import { useState, useMemo, useRef, useCallback, useEffect } from 'react';
-// const images = [];
+const images = [
+    '/carousel_final_1728_webp/Stadler_Carousel_000.png',
+    '/carousel_final_1728_webp/Stadler_Carousel_001.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_002.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_003.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_004.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_005.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_006.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_007.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_008.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_009.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_010.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_011.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_012.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_013.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_014.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_015.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_016.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_017.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_018.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_019.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_020.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_021.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_022.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_023.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_024.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_025.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_026.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_027.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_028.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_029.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_030.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_031.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_032.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_033.png',
+    '/carousel_final_1728_webp/Stadler_Carousel_034.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_035.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_036.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_037.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_038.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_039.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_040.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_041.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_042.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_043.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_044.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_045.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_046.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_047.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_048.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_049.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_050.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_051.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_052.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_053.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_054.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_055.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_056.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_057.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_058.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_059.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_060.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_061.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_062.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_063.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_064.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_065.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_066.png',
+    '/carousel_final_1728_webp/Stadler_Carousel_067.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_068.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_069.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_070.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_071.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_072.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_073.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_074.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_075.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_076.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_077.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_078.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_079.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_080.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_081.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_082.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_083.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_084.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_085.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_086.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_087.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_088.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_089.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_090.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_091.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_092.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_093.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_094.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_095.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_096.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_097.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_098.webp',
+    '/carousel_final_1728_webp/Stadler_Carousel_099.webp',
+];
 
 const onDraw = (img, ctx) => {
     const canvas = ctx.canvas;
@@ -8,7 +109,6 @@ const onDraw = (img, ctx) => {
     const ratio = Math.max(widthRatio, heightRatio);
     const centerX = (canvas.width - img.width * ratio) / 2;
     const centerY = (canvas.height - img.height * ratio) / 2;
-    // console.log("ratio", ratio, widthRatio, heightRatio);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(
         img,
@@ -24,14 +124,17 @@ const onDraw = (img, ctx) => {
 };
 
 const ImageSequence = ({ category }) => {
+    const [prevCategoryNumber, categoryNumberChange] = useState(category);
     const canvasRef = useRef(null);
     const progress = 0;
 
-    let imageWidth;
-    let imageFormat;
+    console.log('progress', progress, category, prevCategoryNumber);
+
+    // let imageWidth;
+    // let imageFormat;
 
     const createImage = (src) => {
-        console.log('src', src);
+        // console.log('src', src);
         if (typeof document !== 'undefined') {
             const img = document.createElement('img');
             img.src = src;
@@ -41,29 +144,25 @@ const ImageSequence = ({ category }) => {
 
     const keyframes = useMemo(() => {
         return [...new Array(100)].map((_, i) => {
-            if (typeof window !== 'undefined') {
-                imageWidth =
-                    window?.innerWidth > 1740
-                        ? 'carousel_final_1728_webp'
-                        : window?.innerWidth > 600
-                          ? 'carousel_final_1440_webp'
-                          : 'carousel_final_600_png';
+            // if (typeof window !== 'undefined') {
+            //     imageWidth =
+            //         window?.innerWidth > 1740
+            //             ? 'carousel_final_1728_webp'
+            //             : window?.innerWidth > 600
+            //               ? 'carousel_final_1440_webp'
+            //               : 'carousel_final_600_png';
 
-                if (i === 0 || i === 34 || i === 67 || i === 99) {
-                    imageFormat = 'webp';
-                } else {
-                    imageFormat = 'webp';
-                }
-            } else {
-                imageWidth = 'carousel_final_1728_webp';
-                imageFormat = 'webp';
-            }
+            //     if (i === 0 || i === 34 || i === 67 || i === 99) {
+            //         imageFormat = 'webp';
+            //     } else {
+            //         imageFormat = 'webp';
+            //     }
+            // } else {
+            //     imageWidth = 'carousel_final_1728_webp';
+            //     imageFormat = 'webp';
+            // }
 
-            return createImage(
-                `/${imageWidth}/Stadler_Carousel_${i
-                    .toString()
-                    .padStart(3, '0')}.${imageFormat}`
-            );
+            return createImage(images[i]);
         });
     }, []);
 
@@ -82,8 +181,10 @@ const ImageSequence = ({ category }) => {
             const constraint = (n, min = 0, max = keyframes.length - 1) =>
                 Math.min(Math.max(n, min), max);
             console.log(
-                Math.round(keyframes.length * progress),
-                keyframes[constraint(Math.round(keyframes.length * progress))]
+                'render image 183',
+                progress,
+                category,
+                prevCategoryNumber
             );
             onDraw(
                 keyframes[constraint(Math.round(keyframes.length * progress))],
@@ -96,6 +197,12 @@ const ImageSequence = ({ category }) => {
     useEffect(() => {
         resizeCanvas();
         const resizeCanvasAndRerender = () => {
+            console.log(
+                'resize canvas',
+                progress,
+                category,
+                prevCategoryNumber
+            );
             resizeCanvas();
             renderImage(progress);
         };
@@ -120,12 +227,11 @@ const ImageSequence = ({ category }) => {
         keyframes.forEach((image) => {
             const img = new Image();
             img.src = image;
-            console.log('image', image);
+            // console.log('image', image);
         });
     }, []);
 
-    const [prevCategoryNumber, categoryNumberChange] = useState(category);
-    const [lastOfClickedCategories, changeLastOfClickedCategories] = useState();
+    // const [lastOfClickedCategories, changeLastOfClickedCategories] = useState();
 
     const [animationProgress, animationProgressChange] = useState(false);
 
@@ -159,7 +265,6 @@ const ImageSequence = ({ category }) => {
                 done = true;
                 animationProgressChange(false);
                 categoryNumberChange(category);
-                console.log('end of animation', lastOfClickedCategories);
             } else {
                 // If animation is not yet finished, call the animation function again
                 if (!done) {
@@ -189,7 +294,13 @@ const ImageSequence = ({ category }) => {
     };
 
     useEffect(() => {
-        console.log('category', category);
+        console.log(
+            'category',
+            category,
+            prevCategoryNumber,
+            previousCategory,
+            newCategory
+        );
         if (!animationProgress) {
             if (previousCategory != newCategory) {
                 animationProgressChange(true);
@@ -199,7 +310,7 @@ const ImageSequence = ({ category }) => {
             }
         } else {
             // Here the logic of
-            changeLastOfClickedCategories(category);
+            // changeLastOfClickedCategories(category);
         }
     }, [category]);
 
@@ -212,10 +323,13 @@ const ImageSequence = ({ category }) => {
     );
 };
 
-const TrainSequence = ({ selectedCategory }) => (
-    <div className="overflow-clip">
-        <ImageSequence category={selectedCategory} />
-    </div>
-);
+const TrainSequence = ({ selectedCategory }) => {
+    console.log('selectedCategory', selectedCategory);
+    return (
+        <div className="overflow-clip">
+            <ImageSequence category={selectedCategory} />
+        </div>
+    );
+};
 
 export default TrainSequence;
