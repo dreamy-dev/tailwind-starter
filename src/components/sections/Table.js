0a88420c3620fb2785ae187af0412bdb0174d738
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-10-01 10:37:55 +0200

    💄 Harmonize table styles

--- src/components/sections/Table.js
+++ src/components/sections/Table.js
@@ -42,7 +42,7 @@ const TableGrid = ({ blok }) => {
                                         {tr.body.map((td, colIndex) => (
                                             <td
                                                 key={colIndex}
-                                                className="px-6 py-4"
+                                                className="px-6 py-4 text-base text-black"
                                             >
                                                 {td.value}
                                             </td>
