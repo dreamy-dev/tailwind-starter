9cf26237c298d7c6cbc533855f562407155e42a5
Cécile <49187442+dreamy-dev@users.noreply.github.com>
2024-11-11 10:26:34 +0100

    Remove twitter feed

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -7,7 +7,6 @@ import {
 import Layout from '/src/components/sections/Layout';
 import { redirect } from 'next/navigation';
 import SocialMediaFeedLinkedIn from '../../components/sections/SocialMediaFeedLinkedIn';
-import SocialMediaFeedTwitter from '../../components/sections/SocialMediaFeedTwitter';
 
 storyblokInit({
     accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
@@ -134,7 +133,6 @@ export default async function Homepage({ params }) {
             <Layout config_footer={config_footer} config_header={config_header}>
                 <StoryblokStory story={story} />
                 <SocialMediaFeedLinkedIn />
-                <SocialMediaFeedTwitter />
             </Layout>
         </>
     );
