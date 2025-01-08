f2f3e4b1ebcdef7e6eb827e7a320c7c359878589
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2025-01-08 11:52:48 +0100

    delete: bg color

--- src/components/sections/SocialWallComponent.js
+++ src/components/sections/SocialWallComponent.js
@@ -4,18 +4,20 @@ import H2 from '../typography/H2';
 
 const SocialWallComponent = ({ blok }) => {
     return (
-        <section {...storyblokEditable(blok)} className="bg-white py-16 antialiased lg:py-24">
+        <section
+            {...storyblokEditable(blok)}
+            className="py-16 antialiased lg:py-24"
+        >
             <ContentWidth>
                 <div className="col-span-12">
                     <H2>{blok?.title}</H2>
-                    <iframe 
-                      src="https://www.juicer.io/api/feeds/stadler_rail/iframe" 
-                      frameborder="0" 
-                      width="100%" 
-                      height="800" 
-                      style={{display: "block"}}
-                    >
-                    </iframe>
+                    <iframe
+                        src="https://www.juicer.io/api/feeds/stadler_rail/iframe"
+                        frameborder="0"
+                        width="100%"
+                        height="800"
+                        style={{ display: 'block' }}
+                    ></iframe>
                 </div>
             </ContentWidth>
         </section>
