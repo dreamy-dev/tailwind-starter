be728abed20aeec29570c1281d36b11eaec63bec
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2025-01-08 08:52:23 +0100

    fix: icons in highlight section

--- src/components/sections/AdvantageItem.js
+++ src/components/sections/AdvantageItem.js
@@ -5,7 +5,12 @@ import H4 from '../typography/H4';
 const AdvantageItem = ({ blok }) => (
     <div {...storyblokEditable(blok)}>
         <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primaryTrans-100 lg:h-12 lg:w-12">
-            {blok?.icon.filename}
+            <img
+                width={150}
+                height={150}
+                src={blok?.icon.filename}
+                alt={blok?.icon.alt || 'Highlighted Icon'}
+            />
         </div>
         <H4>{blok?.Title}</H4>
         <p className="text-greySolid-600">{blok?.Text}</p>
