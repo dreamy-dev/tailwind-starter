8095cf74711c5c0d43cb7d32a7cb181870c5d0cb
alexej1900 <alexej1900@gmail.com>
2025-01-16 10:46:39 +0100

    remove: controls in video

--- src/components/sections/VideoFullWidth.js
+++ src/components/sections/VideoFullWidth.js
@@ -45,7 +45,7 @@ export default function VideoFullWidth({ blok }) {
                 <video
                     loading="lazy"
                     ref={videoRef}
-                    controls={true}
+                    controls={false}
                     autoPlay={true}
                     playsInline={true}
                     muted={true}
